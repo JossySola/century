5d3d1d311cba85d92b5866b479e1f24022e92761
Jossy Solá <jossysola@gmail.com>
2024-06-14 23:12:05 -0600

    Sets Effect hook with setInterval to clear cache and retrieves loader data

--- src/organisms/Feed/Feed.tsx
+++ src/organisms/Feed/Feed.tsx
@@ -1,8 +1,28 @@
 import React, { useEffect, useState } from "react"
+import { useLoaderData } from "react-router-dom"
 import "./Feed.css"
-
  
-export default function Feed ({t3}) {
+export default function Feed () {
+    const data: any = useLoaderData();
+    
+    useEffect(() => {
+        const timer = setInterval(() => {
+            caches.keys().then(keys => {
+                for (const key of keys) {
+                    const isOurCache = key.startsWith("century-");
+                    if (isOurCache) {
+                        caches.open(key).then(cache => {
+                            cache.delete(data.url).then(response => console.log(response))
+                        })
+                    }
+                }
+            })
+        }, 300000);
+
+        return () => clearInterval(timer);
+    }, [])
+
+    console.log(data)
 
     return (<></>)
 }
