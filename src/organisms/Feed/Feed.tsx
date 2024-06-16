beb2a35a070a4c54e2c2482bd43d3365d7a6f31e
Jossy Solá <jossysola@gmail.com>
2024-06-16 15:27:56 -0600

    Adds interface Article and maps loader data to render each article

--- src/organisms/Feed/Feed.tsx
+++ src/organisms/Feed/Feed.tsx
@@ -1,7 +1,28 @@
 import React, { useEffect, useState } from "react"
 import { useLoaderData } from "react-router-dom"
+import Preview from "../../molecules/Preview/Preview";
 import "./Feed.css"
  
+interface Article {
+    author: string;
+    author_fullname: string;
+    created_utc: number;
+    downs: number;
+    id: string;
+    name: string;
+    num_comments: number;
+    permalink: string;
+    preview: object;
+    selftext: string;
+    selftext_html: string | null;
+    subreddit: string;
+    subreddit_id: string;
+    subreddit_name_prefixed: string;
+    title: string;
+    ups: number;
+    url: string;
+}
+
 export default function Feed () {
     const data: any = useLoaderData();
     
@@ -21,8 +42,16 @@ export default function Feed () {
 
         return () => clearInterval(timer);
     }, [])
-
-    console.log(data)
-
-    return (<></>)
+    
+    return (
+        <main>
+            {
+                data.articles && data.articles.map((article: Article) => {
+                    if (article.author !== "[deleted]") {
+                        return <Preview payload={article} key={article.id}/>
+                    }
+                })
+            }
+        </main>
+    )
 }
