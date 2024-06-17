ea366f1788ba7417ec8a4f0671de95cab60f83c5
Jossy Solá <jossysola@gmail.com>
2024-06-16 19:30:09 -0600

    Creates new component

---
+++ src/organisms/Post/Post.stories.tsx
@@ -0,0 +1,17 @@
+import type {Meta, StoryObj} from "@storybook/react"
+import Post from "./Post.tsx"
+ 
+const meta: Meta <typeof Post> = {
+title: "",
+component: Post,
+parameters: {
+ 
+}
+}
+export default meta
+ 
+type Story = StoryObj <typeof Post>
+ 
+export const Default: Story = {
+ 
+}
---
+++ src/organisms/Post/Post.test.js
@@ -0,0 +1,2 @@
+import {describe, expect, test} from "@jest/globals"
+import Post from "./Post.tsx"
