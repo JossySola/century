9e208966c763b735e6ec499f464c75d7f29a3c00
Jossy Solá <jossysola@gmail.com>
2024-06-16 23:27:08 -0600

    Implements code to render Post and Comments components

--- src/organisms/Post/Post.tsx
+++ src/organisms/Post/Post.tsx
@@ -1,7 +1,34 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+
+import fetchHandler from "../../cache/hook"
+import redditFilter from "../../scripts/redditFilter/redditFilter"
+
+import Post_Content from "../../molecules/Post_Content/Post_Content"
+import Comments from "../../molecules/Comments/Comments"
+
 import "./Post.css"
+
  
-export default function Post ({props}) {
+export default function Post () {
+    const {subreddit, id, title} = useParams();
+    const [data, setData] = useState();
+
+    const url = `https://www.reddit.com/r/${subreddit}/comments/${id}/${title}.json`;
+
+    useEffect(() => {
+        fetchHandler(url).then(response => setData(redditFilter(response)));
+    }, [])
  
-return (<></>)
+    return (
+        <article>
+            {
+                data && 
+                <>
+                    <Post_Content t3={data[0][0]}/>
+                    <Comments t1={data[1][0]} />
+                </>
+            }
+        </article>
+    )
 }
