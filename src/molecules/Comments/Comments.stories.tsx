5720bfd9031a09f68f148a3b7f5b9461ab2ebc4d
Jossy Solá <jossysola@gmail.com>
2024-06-16 23:24:16 -0600

    Creates React component

---
+++ src/molecules/Comments/Comments.stories.tsx
@@ -0,0 +1,17 @@
+import type {Meta, StoryObj} from "@storybook/react"
+import Comments from "./Comments.tsx"
+ 
+const meta: Meta <typeof Comments> = {
+title: "",
+component: Comments,
+parameters: {
+ 
+}
+}
+export default meta
+ 
+type Story = StoryObj <typeof Comments>
+ 
+export const Default: Story = {
+ 
+}
---
+++ src/molecules/Comments/Comments.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Comments.css"
+ 
+export default function Comments ({t1}) {
+ 
+    return (
+        <section></section>
+    )
+}
