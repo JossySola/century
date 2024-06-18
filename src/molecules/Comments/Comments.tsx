d21db103eb706ee7f5378c44aa0213846ee3c31b
Jossy Solá <jossysola@gmail.com>
2024-06-18 00:39:54 -0600

    Implements the rendering of the individual comments. Creates interface

--- src/molecules/Comments/Comments.tsx
+++ src/molecules/Comments/Comments.tsx
@@ -1,9 +1,42 @@
 import React from "react"
+import Comment from "../../atoms/Comment/Comment"
 import "./Comments.css"
- 
+
+interface Prop {
+    author: string,
+    author_fullname: string,
+    body_html: string,
+    created_utc: number,
+    depth: number,
+    downs: number,
+    id: string,
+    link_id: string,
+    name: string,
+    parent_id: string,
+    replies: {
+        data: {
+            children: Array<object>
+        }
+    } | string,
+    send_replies: boolean,
+    ups: number
+}
 export default function Comments ({t1}) {
- 
+    
     return (
-        <section></section>
+        <section>
+            {t1 && t1.map((comment: Prop) => {
+                if (comment) {
+                    return <Comment 
+                    key={comment.id}
+                    id={comment.id} 
+                    author={comment.author} 
+                    body_html={comment.body_html} 
+                    downs={comment.downs} 
+                    ups={comment.ups}
+                    replies={typeof comment.replies !== "string" && comment.replies.data.children.length > 1 ? comment.replies.data.children.length-1 : 0}/>
+                }
+            })}
+        </section>
     )
 }
