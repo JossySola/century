e20b86b4306c051e2e6062e580713a15a33a447d
Jossy Solá <jossysola@gmail.com>
2024-06-18 15:53:51 -0600

    Adds Infinite Scroll handler implementation, sets UseEffect to load first 5 comments if they exist and configures a minor CSS styling for inverted scroll

--- src/molecules/Comments/Comments.tsx
+++ src/molecules/Comments/Comments.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import Comment from "../../atoms/Comment/Comment"
 import "./Comments.css"
 
@@ -21,22 +21,68 @@ interface Prop {
     send_replies: boolean,
     ups: number
 }
+type stateArray = Array<React.JSX.Element>;
+
 export default function Comments ({t1}) {
+    const [comments, setComments] = useState<stateArray>([]);
+
+    useEffect(() => {
+        let firstLoad: Array<React.JSX.Element> = [];
+
+        for (let i: number = 0; i < 5; i++) {
+                const comment: Prop = t1[i];
+                if (comment) {
+                    firstLoad.push(<Comment 
+                        key={comment.id}
+                        id={comment.id} 
+                        author={comment.author} 
+                        body_html={comment.body_html} 
+                        downs={comment.downs} 
+                        ups={comment.ups}
+                        replies={typeof comment.replies !== "string" && comment.replies.data.children.length > 0 ? comment.replies.data.children.length-1 : 0}/>)
+                } else {
+                    break;
+                }
+        }
+        setComments(firstLoad);
+    }, [])
+    
     
     return (
-        <section>
-            {t1 && t1.map((comment: Prop) => {
-                if (comment && comment.author !== "[deleted]") {
-                    return <Comment 
+        <section id="comments-section" onScroll={() => handleInfiniteScroll(t1, comments, setComments)}>
+            {comments}
+        </section>
+    )
+}
+
+const handleInfiniteScroll = (array: Array<Prop>, comments: stateArray, setComments: React.Dispatch<React.SetStateAction<stateArray>>) => {
+    const section = document.getElementById("comments-section");
+    const currentPos = section && section.scrollHeight ? section.offsetHeight - section.scrollTop : 0;
+    const scrollHeight = section ? section.scrollHeight : 0;
+    let numberOfNodes = section ? section.children.length: 0;
+
+    
+
+    if (currentPos === scrollHeight + 16) {
+        let addArray: stateArray = [];
+
+        for (let i: number = 1; i < 6; i++) {
+            const index: number = i + numberOfNodes;
+            const comment: Prop = array[index];
+
+            if (comment) {
+                addArray.push(<Comment 
                     key={comment.id}
                     id={comment.id} 
                     author={comment.author} 
                     body_html={comment.body_html} 
                     downs={comment.downs} 
                     ups={comment.ups}
-                    replies={typeof comment.replies !== "string" && comment.replies.data.children.length > 0 ? comment.replies.data.children.length-1 : 0}/>
-                }
-            })}
-        </section>
-    )
+                    replies={typeof comment.replies !== "string" && comment.replies.data.children.length > 0 ? comment.replies.data.children.length-1 : 0}/>)
+            } else {
+                break;
+            }
+        }
+        return setComments(comments.concat(addArray));
+    }
 }
