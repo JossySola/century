25fbb0b611bb1da5889a4085e2b93d9d9dc6e1b0
Jossy Solá <jossysola@gmail.com>
2024-06-19 13:44:32 -0600

    Uses redditFilter function to filter the 'more' prop. Implements logic to include an anchor element to appear when there are replies avaiable to read. Implements logic to map and render the replies.

--- src/atoms/Comment/Comment.tsx
+++ src/atoms/Comment/Comment.tsx
@@ -1,12 +1,16 @@
-import React from "react"
+import React, { useState } from "react"
 import { useProfilePicture, useHTMLText } from "../../scripts/custom_hooks/hooks"
+import redditFilter from "../../scripts/redditFilter/redditFilter"
+import { Prop } from "../../molecules/Comments/Comments"
 import "./Comment.css"
  
-export default function Comment ({author, body_html, id, downs, ups, replies}) {
+export default function Comment ({author, body_html, id, downs, ups, replies, more}) {
+    const [showReplies, setShowReplies] = useState(false);
     const profile = useProfilePicture(author);
-
     body_html && useHTMLText(body_html, id);
 
+    const t1 = redditFilter(more)
+    
     return (
         <div>
             <img src={profile} className="User-img"/>
@@ -15,6 +19,26 @@ export default function Comment ({author, body_html, id, downs, ups, replies}) {
             <span>Ups: {ups} </span>
             <span>Downs: {downs} </span>
             <span>Replies: {replies} </span>
+            { replies !== 0 && t1 && <a onClick={e => {
+                e.preventDefault();
+                showReplies ? setShowReplies(false) : setShowReplies(true);
+            }}>...</a>}
+            {
+                showReplies && t1 ? t1.map((comment: Prop) => {
+                    console.log(comment)
+                    if (comment !== undefined) {
+                        return <Comment 
+                        key={comment.id}
+                        id={comment.id} 
+                        author={comment.author} 
+                        body_html={comment.body_html} 
+                        downs={comment.downs} 
+                        ups={comment.ups}
+                        replies={typeof comment.replies !== "string" && comment.replies.data.children.length > 0 ? comment.replies.data.children.length-1 : 0}
+                        more={comment.replies && comment.replies} />
+                    }
+                }) : null
+            }
         </div>
     )
 }
