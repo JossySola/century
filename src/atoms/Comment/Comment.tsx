a116c846078fb1c03864f6d323e395ef861986a5
Jossy Solá <jossysola@gmail.com>
2024-06-19 13:52:16 -0600

    Changes mapping and rendering of individual Comment components to rendering of the Comments component

--- src/atoms/Comment/Comment.tsx
+++ src/atoms/Comment/Comment.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 import { useProfilePicture, useHTMLText } from "../../scripts/custom_hooks/hooks"
 import redditFilter from "../../scripts/redditFilter/redditFilter"
 import { Prop } from "../../molecules/Comments/Comments"
+import Comments from "../../molecules/Comments/Comments"
 import "./Comment.css"
  
 export default function Comment ({author, body_html, id, downs, ups, replies, more}) {
@@ -24,20 +25,7 @@ export default function Comment ({author, body_html, id, downs, ups, replies, mo
                 showReplies ? setShowReplies(false) : setShowReplies(true);
             }}>...</a>}
             {
-                showReplies && t1 ? t1.map((comment: Prop) => {
-                    console.log(comment)
-                    if (comment !== undefined) {
-                        return <Comment 
-                        key={comment.id}
-                        id={comment.id} 
-                        author={comment.author} 
-                        body_html={comment.body_html} 
-                        downs={comment.downs} 
-                        ups={comment.ups}
-                        replies={typeof comment.replies !== "string" && comment.replies.data.children.length > 0 ? comment.replies.data.children.length-1 : 0}
-                        more={comment.replies && comment.replies} />
-                    }
-                }) : null
+                showReplies && t1 ? <Comments t1={t1} /> : null
             }
         </div>
     )
