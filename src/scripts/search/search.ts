bc75bd4103396ee61e5bfebfca8cd1009e595ed2
Jossy Solá <jossysola@gmail.com>
2024-06-24 19:59:23 -0600

    Creates function implementation to use the subreddits/search endpoint

---
+++ src/scripts/search/search.ts
@@ -0,0 +1,27 @@
+import { v4 as uuidv4 } from "uuid";
+import request from "../request";
+
+export default async function search (query: string) {
+    const base = "https://oauth.reddit.com/subreddits/search?";
+    const access_token: string | null = window.localStorage.getItem("access_token");
+    const params = new URLSearchParams({
+        after: "",
+        before: "",
+        count: "0",
+        limit: "15",
+        q: `${query}`,
+        search_query_id: `${uuidv4()}`,
+        show: "all",
+        show_users: "true",
+        sort: "relevance",
+        typeahead_active: "None"
+    })
+    const payload = {
+        method: "GET" as "GET",
+        headers: {
+            Authorization: `Bearer ${access_token}`
+        }
+    }
+    window.localStorage.setItem("query", query)
+    return request(base + params.toString(), payload);
+}
