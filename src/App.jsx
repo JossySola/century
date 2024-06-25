b0b4b95a40fe39ee981b52444847f68e16c5b7d6
Jossy Solá <jossysola@gmail.com>
2024-06-24 20:01:49 -0600

    Adds Form element

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import { useEffect, useState } from 'react'
-import { Outlet } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Form, Outlet } from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   useEffect(() => {
     const timer = setInterval(() => {
+      console.log("App.jsx: Setting timer...")
       caches.keys().then(keys => {
         for (const key of keys) {
           const isOurCache = key.startsWith("century-");
           if (isOurCache) {
             caches.delete(key);
+            console.log("App.jsx: Cache deleted")
           }
         }
       })
@@ -24,27 +25,12 @@ function App() {
 
   return (
     <>
-    <Outlet />
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Form id="search-form" role="search">
+        <input name='search' id='search' aria-label='Search' placeholder='Search...' type='search' />
+        <button form='search-form' type='submit'>Search!</button>
+      </Form>
+
+      <Outlet />
     </>
   )
 }
