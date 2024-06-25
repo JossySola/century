047c70bbcf4898d5b6b4b8710a88925f91456408
Jossy Solá <jossysola@gmail.com>
2024-06-24 23:16:54 -0600

    Creates React component with <Form> element. Implements a readonly input field with the parent id to be passed to the Action Router

--- src/atoms/Submit/Submit.tsx
+++ src/atoms/Submit/Submit.tsx
@@ -3,11 +3,12 @@ import { Form } from "react-router-dom"
 import "./Submit.css"
  
 export default function Submit ({fullname}) {
- 
+    
     return (
-        <Form id="comment-form" role="comment">
-            <input name="comment" id="submit-comment" aria-label="Comment" placeholder="Write a comment..." type="comment"></input>
-            <input form="comment-form" type="submit">Submit</input>
+        <Form id="comment-form" role="comment" method="post">
+            <input name="comment" id="submit-comment" aria-label="Comment" placeholder="Write a comment..." type="comment" />
+            <input name="fullname" value={fullname} readOnly style={{display: "none"}} />
+            <button form="comment-form" type="submit">Submit</button>
         </Form>
     )
 }
