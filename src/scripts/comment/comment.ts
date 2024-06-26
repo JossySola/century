36f27e030cf7ab75cbd72d04f8396749f70dea4d
Jossy Solá <jossysola@gmail.com>
2024-06-26 00:26:08 -0600

    Moves base URL as a global variable. Tests fetch on api/me.json endpoint

--- src/scripts/comment/comment.ts
+++ src/scripts/comment/comment.ts
@@ -1,23 +1,29 @@
-const base = "https://oauth.reddit.com";
+const base = "https://oauth.reddit.com/api";
 
 export async function comment (thing_id: string, text: string) {
-    const url = `${base}/api/comment`;
+    const url = `${base}/comment`;
     const access_token: string | null = window.localStorage.getItem("access_token");
     const payload = {
         method: 'POST',
         body: new URLSearchParams({
             api_type: "json",
-            recaptcha_token: "",
+            recaptcha_token: "a",
             return_rtjson: "true",
-            richtext_json: "",
-            text,
-            thing_id,
+            text: `${text}`,
+            thing_id: `${thing_id}`,
+            "uh / X-Modhash header": "zee5g23a4ac5a9277f8058a4645a84318b8de053c58ecffd94"
         })
     }
+    
+    const me = await fetch("https://oauth.reddit.com/api/me.json");
+    console.log(await me.json())
+    const response = await fetch(url, payload);
+    console.log(response);
+    return response;
 }
 
 export async function del (id: string) {
-    const url = `${base}/api/del`;
+    const url = `${base}/del`;
     const payload = {
         method: 'POST',
         body: new URLSearchParams({
@@ -27,7 +33,7 @@ export async function del (id: string) {
 }
 
 export async function edit (thing_id: string, text: string) {
-    const url = `${base}/api/editusertext`;
+    const url = `${base}/editusertext`;
     const payload = {
         method: 'POST',
         body: new URLSearchParams({
