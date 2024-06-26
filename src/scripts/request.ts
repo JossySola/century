d912599012fc519062480c3eb0cea52ba455a299
Jossy Solá <jossysola@gmail.com>
2024-06-26 00:26:52 -0600

    Reestructures if statement for request.ok, throwing an Error if false

--- src/scripts/request.ts
+++ src/scripts/request.ts
@@ -4,6 +4,9 @@ import getAuthorization from "./authorization/authorization";
 export default async function request (url: string, payload: object) {
     try {
         const request = await fetchHandler(url, payload);
+        if (request.ok === false) {
+            throw new Error(`Status: ${request.status}. Unauthorized`);
+        }
         return request;
     } catch (error) {
         console.error(error);
