5adb8e9a74f6c8d329ca4018b52698cb94ccd16c
Jossy Solá <jossysola@gmail.com>
2024-06-26 00:17:39 -0600

    Gets 'subreddit' boolean as prop. Restructures return JSX render based on that prop.

--- src/atoms/User/User.tsx
+++ src/atoms/User/User.tsx
@@ -2,16 +2,21 @@ import React from "react"
 import "./User.css"
 
 interface Props {
+    subreddit: string | undefined;
     author: string;
     src: string | undefined;
 }
-export default function User ({author, src}: Props) {
+export default function User ({subreddit, author, src}: Props) {
     
     return (
         <a href={`https://www.reddit.com/u/${author}/`} target="_blank">
-            <section>
+            <section id="user" className={subreddit ? "grid-3" : "grid-2"}>
                 <img src={src} className="User-img"/>
-                <h4>u/{author}</h4>
+                {subreddit ? 
+                <>
+                    <h4 className="subreddit">{subreddit}</h4>
+                    <h4 className="author">{author}</h4>
+                </> : <h4 className="author" style={{fontWeight: 700}}>{author}</h4>}
             </section>
         </a>
     )
