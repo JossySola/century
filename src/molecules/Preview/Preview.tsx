c1cf06cc9c8449f39a87e9ed2e77dcf90caed4f3
Jossy Solá <jossysola@gmail.com>
2024-06-26 00:22:07 -0600

    Adds id depending on the existence of cover image. Sets subreddit prop on User as undefined

--- src/molecules/Preview/Preview.tsx
+++ src/molecules/Preview/Preview.tsx
@@ -1,19 +1,18 @@
 import React, { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 import { useProfilePicture } from "../../scripts/custom_hooks/hooks"
-import fetchHandler from "../../cache/hook"
+import { getRandomAvatar } from "../../scripts/custom_hooks/hooks"
 import Cover from "../../atoms/Cover/Cover"
 import Summary from "../../atoms/Summary/Summary"
 import User from "../../atoms/User/User"
 import "./Preview.css"
  
 export default function Preview ({payload}) {
-    const profile = useProfilePicture(payload.author);
+    const profile = getRandomAvatar();
     const images = payload.preview && payload.preview.images;
-
     return (
-        <article>
-            <User  author={payload.author} src={profile} />
+        <article id={images ? "preview-big" : "preview-small"} className="preview">
+            <User  subreddit={undefined} author={payload.author} src={profile} />
             <Link to={payload.permalink}>
                 <Cover preview={images ? images : null} />
                 <Summary  title={payload.title}/>
