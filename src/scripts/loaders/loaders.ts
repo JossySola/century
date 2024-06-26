f1af5b84b4271fa631c15f0bf1b9f2f89e6ee8d3
Jossy Solá <jossysola@gmail.com>
2024-06-26 00:24:01 -0600

    Adds loaders for different categories

--- src/scripts/loaders/loaders.ts
+++ src/scripts/loaders/loaders.ts
@@ -7,6 +7,7 @@ export async function index({request}) {
     const pathname = new URL(request.url);
     const query = pathname.searchParams.get("search");
     const code = pathname.searchParams.get("code");
+    const error = pathname.searchParams.get("error");
     const stateSent = window.localStorage.getItem("state");
     const stateReceived = pathname.searchParams.get("state");
     const temporal = window.localStorage.getItem("query");
@@ -34,10 +35,45 @@ export async function index({request}) {
     const feed = await fetchHandler(url);
     const elements = redditFilter(feed);
     
-    return {elements, url};
+    return {elements, url, error};
 }
 export async function news() {
     const url = "https://www.reddit.com/r/worldnews.json?raw_json=1";
+    const feed = await fetchHandler(url);
+    const elements = redditFilter(feed);
+    
+    return {elements, url};
+}
+export async function gaming() {
+    const url = "https://www.reddit.com/r/gaming.json?raw_json=1";
+    const feed = await fetchHandler(url);
+    const elements = redditFilter(feed);
+    
+    return {elements, url};
+}
+export async function space() {
+    const url = "https://www.reddit.com/r/space.json?raw_json=1";
+    const feed = await fetchHandler(url);
+    const elements = redditFilter(feed);
+    
+    return {elements, url};
+}
+export async function sports() {
+    const url = "https://www.reddit.com/r/sports.json?raw_json=1";
+    const feed = await fetchHandler(url);
+    const elements = redditFilter(feed);
+    
+    return {elements, url};
+}
+export async function science() {
+    const url = "https://www.reddit.com/r/science.json?raw_json=1";
+    const feed = await fetchHandler(url);
+    const elements = redditFilter(feed);
+    
+    return {elements, url};
+}
+export async function tech() {
+    const url = "https://www.reddit.com/r/technology.json?raw_json=1";
     const feed = await fetchHandler(url);
     const elements = redditFilter(feed);
     
