9a2533b2a70e370b9d14542aadb7469e8b9f867b
Jossy Solá <jossysola@gmail.com>
2024-06-26 18:47:22 -0600

    Wraps User and title inside 'content-user'. Inserts icons. Wraps images inside section 'content-interactions'

--- src/molecules/Post_Content/Post_Content.tsx
+++ src/molecules/Post_Content/Post_Content.tsx
@@ -1,6 +1,11 @@
 import React from "react"
 import { useProfilePicture } from "../../scripts/custom_hooks/hooks"
-import User from "../../atoms/User/User";
+import User from "../../atoms/User/User"
+import comment_icon from "../../assets/icons/comment_icon.svg"
+import like from "../../assets/icons/like.svg"
+import dislike from "../../assets/icons/dislike.svg"
+import open_tab_white from "../../assets/icons/open_tab_white.svg"
+
 import "./Post_Content.css"
  
 export default function Post_Content ({t3}) {
@@ -11,22 +16,23 @@ export default function Post_Content ({t3}) {
     
     return (
         <section id="content">
-            <User subreddit={t3.subreddit_name_prefixed} author={t3.author} src={profile} />
+            <div className="content-user">
+                <User subreddit={t3.subreddit_name_prefixed} author={t3.author} src={profile} />
+                <h2>{t3.title}</h2>
+            </div>
             
-            <h2>{t3.title}</h2>
             
             
             {t3.selftext && <p>{t3.selftext}</p>}
 
             <div className="content-bottom">
-                <section>
-                    <img /><span>Ups:{t3.ups} </span>
-                    <img /><span>Downs:{t3.downs} </span>
-                    <img /><span>Comments:{t3.num_comments} </span>
+                <section className="content-interactions">
+                    <img src={like} alt="upvotes"/><span>{t3.ups} </span>
+                    <img src={dislike} alt="downvotes"/><span>{t3.downs} </span>
+                    <img src={comment_icon} alt="comments"/><span>{t3.num_comments} </span>
                 </section>
-                <a href={t3.url} target="_blank" className="secondary">Open article</a>
+                <a href={t3.url} target="_blank" className="secondary">Open article <img src={open_tab_white} /></a>
             </div>
-            
         </section>
     )
 }
