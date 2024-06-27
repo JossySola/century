0408a534e5dd37d29e954c896289443d48e03081
Jossy Solá <jossysola@gmail.com>
2024-06-26 18:43:03 -0600

    Adds icons to upvotes, downvotes and comments data

--- src/atoms/Comment/Comment.tsx
+++ src/atoms/Comment/Comment.tsx
@@ -1,7 +1,12 @@
 import React, { useState } from "react"
 import { useProfilePicture, useHTMLText } from "../../scripts/custom_hooks/hooks"
+import Replies from "../../molecules/Replies/Replies"
+
 import redditFilter from "../../scripts/redditFilter/redditFilter"
-import Comments from "../../molecules/Comments/Comments"
+import dislike from "../../assets/icons/dislike.svg"
+import like from "../../assets/icons/like.svg"
+import comment_icon from "../../assets/icons/comment_icon.svg"
+
 import "./Comment.css"
  
 export default function Comment ({author, body_html, id, downs, ups, replies, more}) {
@@ -13,19 +18,31 @@ export default function Comment ({author, body_html, id, downs, ups, replies, mo
     
     return (
         <div className="comment-div">
-            <img src={profile} className="User-img"/>
-            <h4>u/{author}</h4>
-            <div id={id}></div>
-            <span>Ups: {ups} </span>
-            <span>Downs: {downs} </span>
-            <span>Replies: {replies} </span>
-            { replies !== 0 && t1 && <a onClick={e => {
-                e.preventDefault();
-                showReplies ? setShowReplies(false) : setShowReplies(true);
-            }}>...</a>}
+            <div className="comment">
+                <img src={profile} className="User-img"/>
+
+                <div className="content">
+                    <h4>u/{author}</h4>
+                    <div id={id}></div>
+
+                    <div className="comment-interactions">
+                        <span><img src={like} alt="upvotes" decoding="sync"/> {ups} </span>
+                        <span><img src={dislike} alt="downvotes" decoding="sync"/> {downs} </span>
+                        <span><img src={comment_icon} alt="comments" decoding="sync"/> {replies} </span>
+                    </div>
+                    
+
+                    { replies !== 0 && t1 && <a onClick={e => {
+                        e.preventDefault();
+                        showReplies ? setShowReplies(false) : setShowReplies(true);
+                    }}>...</a>}
+                    
+                </div>
+            </div>
             {
-                showReplies && t1 ? <Comments t1={t1} fullname={undefined} /> : null
+                showReplies && t1 ? <Replies t1={t1} /> : null
             }
         </div>
+        
     )
 }
