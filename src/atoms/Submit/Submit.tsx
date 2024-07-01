7bea6e244485cb36342b81d3221086cf01ffdb16
Jossy Solá <jossysola@gmail.com>
2024-07-01 01:16:46 -0600

    Adds class names to Form and Input. Changes button name from Submit to Comment

--- src/atoms/Submit/Submit.tsx
+++ src/atoms/Submit/Submit.tsx
@@ -5,10 +5,10 @@ import "./Submit.css"
 export default function Submit ({fullname}) {
     
     return (
-        <Form id="comment-form" role="comment" method="post">
-            <input name="comment" id="submit-comment" aria-label="Comment" placeholder="Write a comment..." type="comment" />
+        <Form id="comment-form" role="comment" method="post" className="submit-form">
+            <input name="comment" id="submit-comment" aria-label="Comment" placeholder="Write a comment..." type="comment" className="submit-input"/>
             <input name="fullname" value={fullname} readOnly style={{display: "none"}} />
-            <button form="comment-form" type="submit">Submit</button>
+            <button form="comment-form" type="submit">Comment</button>
         </Form>
     )
 }
