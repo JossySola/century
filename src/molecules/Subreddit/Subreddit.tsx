2a2412fa4dba64fec757ebc57493cd11d763fe05
Jossy Solá <jossysola@gmail.com>
2024-07-01 01:23:22 -0600

    Adds class names and wraps img with h3 inside 'subreddit-identity'.

--- src/molecules/Subreddit/Subreddit.tsx
+++ src/molecules/Subreddit/Subreddit.tsx
@@ -1,26 +1,29 @@
 import React from "react"
 import { useHTMLText } from "../../scripts/custom_hooks/hooks"
+import formatAmount from "../../scripts/amount";
 import "./Subreddit.css"
  
 export default function Subreddit ({payload}) {
     useHTMLText(payload.public_description_html, payload.id);
     
     return (
-        <section>
+        <section className="subreddit">
             <a href={`https://www.reddit.com${payload.url}`} target="_blank">
-                <img src={payload.banner_img} style={{
+                <img src={payload.banner_img} className="subreddit-banner" style={{
                     backgroundColor: payload.banner_background_color
                 }}/>
-                <img src={payload.icon_img} style={{
-                    backgroundColor: payload.key_color,
-                    borderRadius: "50%",
-                    width: 64
-                }}/>
-                <h3>{payload.header_title}</h3>
                 
+                <div className="subreddit-identity">
+                    <img src={payload.icon_img} className="subreddit-icon" style={{
+                        backgroundColor: payload.key_color,
+                        borderRadius: "50%",
+                        width: 64
+                    }}/>
+                    <h3>{payload.display_name_prefixed}</h3>
+                </div>
                 
                 <p id={payload.id}></p>
-                <p>{payload.subscribers} members</p>
+                <p style={{color: "#656869", textAlign: "center"}}>{formatAmount(payload.subscribers)} members</p>
             </a>
         </section>
     )
