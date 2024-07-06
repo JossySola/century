e7c8098b9ce19b7508910b0f21fbb11d95644b03
Jossy Solá <jossysola@gmail.com>
2024-07-06 01:19:02 -0600

    Adds cache removal logic when new comment is submited to gather the comments update

--- src/scripts/actions/actions.ts
+++ src/scripts/actions/actions.ts
@@ -2,18 +2,27 @@ import {comment as commentAction} from "../comment/comment";
 
 export async function submitComment ({request}) {
     const data = Object.fromEntries(await request.formData());
+    const url = new URL(request.url);
+    const pathname = url.pathname;
+    const link = pathname.slice(1,pathname.length-1)
     const comment = data['comment'];
     const parent = data['fullname'];
     const input= document.getElementById("submit-comment") as HTMLInputElement;
+    input ? input.value = "" : null;
 
     if (!data['comment']) {
         return null;
     }
     
     const response = await commentAction(parent, comment);
-    console.log(response)
 
-    input ? input.value = "" : null;
+    await caches.keys().then(keys => {
+        for (const key of keys) {
+            caches.open(key).then(async (cache) => {
+                await cache.delete(`https://www.reddit.com/${link}.json`);
+            }) 
+        }
+    })
 
-    return null;
+    return response;
 }
