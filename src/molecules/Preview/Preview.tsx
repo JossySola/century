37b91a4e6816a2d667c6e8f9d0acb5b037c20d50
Jossy Solá <jossysola@gmail.com>
2024-07-09 11:46:45 -0600

    Removes imports of Avatar PNG images

--- src/molecules/Preview/Preview.tsx
+++ src/molecules/Preview/Preview.tsx
@@ -5,14 +5,6 @@ import Cover from "../../atoms/Cover/Cover"
 import Summary from "../../atoms/Summary/Summary"
 import User from "../../atoms/User/User"
 
-import avatar_default_1 from "../../assets/icons/avatar/avatar_default_1.png"
-import avatar_default_2 from "../../assets/icons/avatar/avatar_default_2.png"
-import avatar_default_3 from "../../assets/icons/avatar/avatar_default_3.png"
-import avatar_default_4 from "../../assets/icons/avatar/avatar_default_4.png"
-import avatar_default_5 from "../../assets/icons/avatar/avatar_default_5.png"
-import avatar_default_6 from "../../assets/icons/avatar/avatar_default_6.png"
-import avatar_default_7 from "../../assets/icons/avatar/avatar_default_7.png"
-
 import "./Preview.css"
  
 export default function Preview ({payload}) {
