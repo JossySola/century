af069cdbe8438b50c7ab3a8938717d46b8aabe77
Jossy Solá <jossysola@gmail.com>
2024-07-08 23:03:53 -0600

    Completes with a dot each relative pathname

--- src/scripts/custom_hooks/hooks.js
+++ src/scripts/custom_hooks/hooks.js
@@ -169,27 +169,27 @@ export const getRandomAvatar = () => {
     const random = Math.floor(Math.random()*7);
     switch (random) {
         case 1:
-            return "/src/assets/avatar/avatar_default_1.png";
+            return "./src/assets/avatar/avatar_default_1.png";
             break;
         case 2:
-            return "/src/assets/avatar/avatar_default_2.png";
+            return "./src/assets/avatar/avatar_default_2.png";
             break;
         case 3:
-            return "/src/assets/avatar/avatar_default_3.png";
+            return "./src/assets/avatar/avatar_default_3.png";
             break;
         case 4:
-            return "/src/assets/avatar/avatar_default_4.png";
+            return "./src/assets/avatar/avatar_default_4.png";
             break;
         case 5:
-            return "/src/assets/avatar/avatar_default_5.png";
+            return "./src/assets/avatar/avatar_default_5.png";
             break;
         case 6:
-            return "/src/assets/avatar/avatar_default_6.png";
+            return "./src/assets/avatar/avatar_default_6.png";
             break;
         case 7:
-            return "/src/assets/avatar/avatar_default_7.png";
+            return "./src/assets/avatar/avatar_default_7.png";
             break;
         default:
-            return "/src/assets/avatar/avatar_default_1.png";
+            return "./src/assets/avatar/avatar_default_1.png";
     }
 }
