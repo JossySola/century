f706b839ae2ef07c96029255ae0cf714fb9768c2
Jossy Solá <jossysola@gmail.com>
2024-07-08 22:36:45 -0600

    Sets an id to Submit button. Includes a State setter passed as Prop to pass event information up

--- src/atoms/Submit/Submit.tsx
+++ src/atoms/Submit/Submit.tsx
@@ -4,14 +4,19 @@ import "./Submit.css"
  
 interface Props {
     fullname: string,
+    setSubmitEvent: React.Dispatch<React.SetStateAction<React.FormEvent<HTMLFormElement>>>
 }
-export default function Submit ({fullname}: Props) {
+export default function Submit ({fullname, setSubmitEvent}: Props) {
     
     return (
-        <Form id="comment-form" role="comment" method="post" className="submit-form">
-            <input name="comment" id="submit-comment" aria-label="Comment" placeholder="Write a comment..." type="comment" className="submit-input"/>
-            <input name="fullname" value={fullname} readOnly style={{display: "none"}} />
-            <button form="comment-form" type="submit">Comment</button>
-        </Form>
+        <>
+            <Form id="comment-form" role="comment" method="post" className="submit-form" onSubmit={(e) => {
+                setSubmitEvent(e);
+            }}>
+                <input name="comment" id="submit-comment" aria-label="Comment" placeholder="Write a comment..." type="comment" className="submit-input"/>
+                <input name="fullname" value={fullname} style={{display: "none"}} readOnly/>
+                <button form="comment-form" id="submit" type="submit">Comment</button>
+            </Form>
+        </>
     )
 }
