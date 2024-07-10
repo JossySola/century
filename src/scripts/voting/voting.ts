d7bfa54e118748e712e71ac5347323ec72127b52
Jossy Solá <jossysola@gmail.com>
2024-07-10 00:03:14 -0600

    Sets code implementation with try...catch statement, including 'getAuthorization' function inside catch

--- src/scripts/voting/voting.ts
+++ src/scripts/voting/voting.ts
@@ -1,10 +1,15 @@
+import getAuthorization from "../authorization/authorization";
 const base = "https://oauth.reddit.com/api";
 
 export async function vote (id: string, dir: "1" | "0" | "-1") {
-    const url = `${base}/api/vote`;
-
+    const url = `${base}/vote`;
+    const access_token: string | null = window.localStorage.getItem("access_token");
     const payload = {
         method: 'POST',
+        headers: {
+            Authorization: `Bearer ${access_token}`,
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
         body: new URLSearchParams({
             dir,
             id,
@@ -12,5 +17,24 @@ export async function vote (id: string, dir: "1" | "0" | "-1") {
             "uh / X-Modhash header": ""
         })
     }
-
+    
+    try {
+        const body = await fetch(url, payload);
+        const response = await body.json();
+        if (response.ok === false) {
+            if (response.success === false) throw new Error("Unsuccessful request from 'vote' function.");
+            if (response.json && response.json.errors.length > 0) {
+                console.error({
+                    error: response.json.errors[0][0],
+                    msg: response.json.errors[0][1]
+                });
+                throw new Error(`${response.json.errors[0][0]}: ${response.json.errors[0][1]}`);
+            }
+            throw new Error("Failed request from 'vote' function.");
+        }
+        return response;
+    } catch (error) {
+        console.error(error);
+        getAuthorization();
+    }
 }
