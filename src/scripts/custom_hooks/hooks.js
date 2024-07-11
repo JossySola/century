039fa2853e3faeaa2ff67063b26cbefe3d8a979c
Jossy Solá <jossysola@gmail.com>
2024-07-11 16:41:20 -0600

    Changes the relative paths and uses environment variables pointing to the Public folder

--- src/scripts/custom_hooks/hooks.js
+++ src/scripts/custom_hooks/hooks.js
@@ -169,27 +169,27 @@ export const getRandomAvatar = () => {
     const random = Math.floor(Math.random()*7);
     switch (random) {
         case 1:
-            return "./assets/icons/avatar/avatar_default_1.png";
+            return `${process.env.PUBLIC_URL}/avatar_default_1.png`;
             break;
         case 2:
-            return "./assets/icons/avatar/avatar_default_2.png";
+            return `${process.env.PUBLIC_URL}/avatar_default_2.png`;
             break;
         case 3:
-            return "./assets/icons/avatar/avatar_default_3.png";
+            return `${process.env.PUBLIC_URL}/avatar_default_3.png`;
             break;
         case 4:
-            return "./assets/icons/avatar/avatar_default_4.png";
+            return `${process.env.PUBLIC_URL}/avatar_default_4.png`;
             break;
         case 5:
-            return "./assets/icons/avatar/avatar_default_5.png";
+            return `${process.env.PUBLIC_URL}/avatar_default_5.png`;
             break;
         case 6:
-            return "./assets/icons/avatar/avatar_default_6.png";
+            return `${process.env.PUBLIC_URL}/avatar_default_6.png`;
             break;
         case 7:
-            return "./assets/icons/avatar/avatar_default_7.png";
+            return `${process.env.PUBLIC_URL}/avatar_default_7.png`;
             break;
         default:
-            return "./assets/icons/avatar/avatar_default_1.png";
+            return `${process.env.PUBLIC_URL}/avatar_default_1.png`;
     }
 }
