5443ab772aac172c0f3608c0944448d34b6012e4
Jossy Solá <jossysola@gmail.com>
2024-07-11 15:43:21 -0600

    Sets an item to localStorage 'tempLink' before getting Authorization

--- src/scripts/actions/actions.ts
+++ src/scripts/actions/actions.ts
@@ -5,12 +5,12 @@ export async function submitComment ({request}) {
     const data = Object.fromEntries(await request.formData());
     const url = new URL(request.url);
     const pathname = url.pathname;
-    const link = pathname.slice(1,pathname.length-1)
+    const link = pathname.slice(1,pathname.length-1);
     const comment = data['comment'];
     const parent = data['fullname'];
     const input= document.getElementById("submit-comment") as HTMLInputElement;
     input ? input.value = "" : null;
-
+    
     if (!data['comment']) {
         return null;
     }
@@ -24,21 +24,7 @@ export async function submitComment ({request}) {
 
     } catch (error) {
         console.error(error);
+        window.localStorage.setItem("tempLink", link);
         getAuthorization();
-    } finally {
-        await caches.keys().then(keys => {
-            for (const key of keys) {
-                caches.open(key).then(async (cache) => {
-                    await cache.delete(`https://www.reddit.com/${link}.json`).then(response => {
-                        if (response) {
-                            console.log("From submitComment Action: cache deleted");
-                        } else {
-                            console.log("From submitComment Action: Cache deletion failed!");
-                        }
-                    })
-                }) 
-            }
-        })
     }
-    
 }
