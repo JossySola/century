d2f3324626ef361711fcb5982bdd77a222c2c30b
Jossy Solá <jossysola@gmail.com>
2024-07-11 18:51:10 -0600

    Adds condition checking if profile return value is of type 'string'

--- src/atoms/User/User.tsx
+++ src/atoms/User/User.tsx
@@ -1,17 +1,24 @@
 import React from "react"
+import { useProfilePicture } from "../../scripts/custom_hooks/hooks.js"
 import "./User.css"
 
 interface Props {
     subreddit: string | undefined;
     author: string;
-    src: string | undefined;
 }
-export default function User ({subreddit, author, src}: Props) {
+export default function User ({subreddit, author}: Props) {
+    const profile = useProfilePicture(author);
     
     return (
         <a href={`https://www.reddit.com/u/${author}/`} target="_blank">
             <section id="user" className={subreddit ? "grid-3" : "grid-2"}>
-                <img src={src} className="User-img"/>
+                {
+                    typeof profile === "number" ?
+                        <img src={`avatar_default_${profile}.png`} className="User-img"/> :
+                        <img src={profile} className="User-img"/>
+                }
+                
+                
                 {subreddit ? 
                 <>
                     <h4 className="user-subreddit">{subreddit}</h4>
