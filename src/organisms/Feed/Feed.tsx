103ec6a4f0c607fe88196307178e9c24448c254d
Jossy Solá <jossysola@gmail.com>
2024-07-11 21:02:31 -0600

    Adds useNavigation hook and reestructures return JSX to include the loading animation

--- src/organisms/Feed/Feed.tsx
+++ src/organisms/Feed/Feed.tsx
@@ -1,10 +1,10 @@
 import React from "react"
-import { useLoaderData, useNavigate } from "react-router-dom"
-//import { useFeedData } from "../../scripts/custom_hooks/hooks.js"
+import { useLoaderData, useNavigate, useNavigation } from "react-router-dom"
 import Preview from "../../molecules/Preview/Preview";
 import Account from "../../molecules/Account/Account";
 import Subreddit from "../../molecules/Subreddit/Subreddit";
 import back_icon from "../../assets/icons/back_icon.svg";
+import loading from "../../assets/icons/loading_feed_light.svg"
 import "./Feed.css"
  
 interface Article {
@@ -68,36 +68,43 @@ type Thing = Article | _Account | _Subreddit;
 export default function Feed () {
     const data: any = useLoaderData();
     const navigate = useNavigate();
-    
+    const navigation = useNavigation();
+
     return (
-        <main id="feed">
-            {data.error && <span style={{color: "rgb(252, 71, 46)"}}>Authorizing the Web App to connect to Reddit is required to Search.</span>}
-            {data.pathname && 
-                <header>
-                    <a onClick={e => {
-                        e.preventDefault();
-                        navigate(-1);
-                    }} style={{gridArea: "back"}}><img src={back_icon as unknown as string} alt="Go Back" aria-label="Go Back"/></a>
-                    <h2 style={{gridArea: "subreddit"}}>{data.pathname}</h2>
-                </header>
-            }
-            {
-                data.elements && data.elements.map((element: Thing) => {
-                    if (element.kind === "t3" as "t3") {
-                        if (element.author !== "[deleted]") {
-                            return <Preview payload={element} key={element.id}/>
+        <>
+            <div className="feed-loading" style={navigation.state === "loading" ? {display: ""} : {display: "none"}}>
+                <img src={loading as unknown as string} />
+            </div>
+
+            <main id="feed" style={navigation.state === "loading" ? {display: "none"} : {display: ""}}>
+                {data.error && <span style={{color: "rgb(252, 71, 46)"}}>Authorizing the Web App to connect to Reddit is required to Search.</span>}
+                {data.pathname && 
+                    <header>
+                        <a onClick={e => {
+                            e.preventDefault();
+                            navigate(-1);
+                        }} style={{gridArea: "back"}}><img src={back_icon as unknown as string} alt="Go Back" aria-label="Go Back"/></a>
+                        <h2 style={{gridArea: "subreddit"}}>{data.pathname}</h2>
+                    </header>
+                }
+                {
+                    data.elements && data.elements.map((element: Thing) => {
+                        if (element.kind === "t3" as "t3") {
+                            if (element.author !== "[deleted]") {
+                                return <Preview payload={element} key={element.id}/>
+                            }
                         }
-                    }
-                    if (element.kind === "t5" as "t5") {
-                        return <Subreddit payload={element} key={element.id} />
-                    }
-                    if (element.kind === "t2" as "t2") {
-                        if (!element.is_blocked) {
-                            return <Account payload={element} key={element.id} />
+                        if (element.kind === "t5" as "t5") {
+                            return <Subreddit payload={element} key={element.id} />
                         }
-                    }
-                })
-            }
-        </main>
+                        if (element.kind === "t2" as "t2") {
+                            if (!element.is_blocked) {
+                                return <Account payload={element} key={element.id} />
+                            }
+                        }
+                    })
+                }
+            </main>
+        </>
     )
 }
