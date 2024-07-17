996676758fe6a32ef33c442d50302d9f08651e4f
Jossy Solá <jossysola@gmail.com>
2024-07-17 01:46:56 -0600

    Implements new state hooks: loggedIn and sortType

--- src/organisms/Post/Post.tsx
+++ src/organisms/Post/Post.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react"
 import { useNavigate, useParams } from "react-router-dom"
-import getPostData from "../../scripts/loaders/getPostData"
+import { usePostDataFetcher } from "../../scripts/custom_hooks/hooks.js";
 import Post_Content from "../../molecules/Post_Content/Post_Content"
 import Comments from "../../molecules/Comments/Comments"
 import { Prop } from "../../molecules/Comments/Comments"
@@ -14,24 +14,27 @@ export default function Post () {
     const {subreddit, id, title} = useParams();
     const [data, setData] = useState<State | undefined>();
     const [loading, setLoading] = useState<boolean>(true);
-    const [submitEvent, setSubmitEvent] = useState<React.FormEvent<HTMLFormElement> | undefined>();
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [sortType, setSortType] = useState<"best" | "new" | "top" | "controversial" | "old">("best");
     const navigate = useNavigate();
 
     const fullname = data && data[0][0].name;
-
+    
     useEffect(() => {
-        getPostData(subreddit, id, title).then(response => {
-            setData(response);
+        usePostDataFetcher(subreddit, id, title, sortType).then(response => {
+            setData(response.JSON);
+            setLoggedIn(response.loggedIn);
             setLoading(false);
         });
         const constantFetch = setInterval(() => {
-            getPostData(subreddit, id, title).then(response => {
-                setData(response);
+            usePostDataFetcher(subreddit, id, title, sortType).then(response => {
+                setData(response.JSON);
+                setLoggedIn(response.loggedIn);
             });
         }, 60000);
 
         return () => clearInterval(constantFetch);
-    }, [submitEvent])
+    }, [sortType])
     
     return (
         <>
@@ -47,12 +50,12 @@ export default function Post () {
                 : null
             }
 
-            <article>
+            <article id="post">
                 {
                     data && 
                     <>
                         <Post_Content t3={data[0][0]}/>
-                        <Comments t1={data[1]} t3={true} fullname={fullname} setSubmitEvent={setSubmitEvent} />
+                        <Comments t1={data[1]} t3={true} fullname={fullname} sortType={sortType} setSortType={setSortType} loggedIn={loggedIn}/>
                     </>
                 }
             </article>
