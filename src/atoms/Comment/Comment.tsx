43560c119c28a97d180db2f517d3144230c2876a
Jossy Solá <jossysola@gmail.com>
2024-07-18 00:15:54 -0600

    Divides interactions into two divs. Adds delete comment button if the comment is made by the user

--- src/atoms/Comment/Comment.tsx
+++ src/atoms/Comment/Comment.tsx
@@ -10,6 +10,7 @@ import liked from "../../assets/icons/liked.svg"
 import plus_icon from "../../assets/icons/plus_icon.svg"
 import minus_icon from "../../assets/icons/minus_icon.svg"
 import comment_icon from "../../assets/icons/comment_icon.svg"
+import bin from "../../assets/icons/bin.svg"
 import { vote } from "../../scripts/voting/voting"
 import avatar1 from "../../assets/avatar_default_1.png"
 import avatar2 from "../../assets/avatar_default_2.png"
@@ -19,14 +20,29 @@ import avatar5 from "../../assets/avatar_default_5.png"
 import avatar6 from "../../assets/avatar_default_6.png"
 import avatar7 from "../../assets/avatar_default_7.png"
 import getCachedOrNewImage from "../../profiles.js"
+import { del } from "../../scripts/comment/comment"
 import "./Comment.css"
  
 
-export default function Comment ({author, body_html, id, depth, downs, ups, likes, name, replies, more}) {
+export default function Comment ({
+    author, 
+    body_html, 
+    id, 
+    depth, 
+    downs, 
+    ups, 
+    likes, 
+    name, 
+    replies, 
+    more, 
+    setTrigger, 
+    trigger}) {
+
     const [showReplies, setShowReplies] = useState(false);
     const [voting, setVoting] = useState(likes === true ? "1" : "0");
     const profile = useProfilePicture(author);
     const avatars = [avatar1, avatar2, avatar3, avatar4, avatar5, avatar6, avatar7];
+    const me = window.sessionStorage.getItem("me");
 
     body_html && useHTMLText(body_html, id);
 
@@ -48,47 +64,59 @@ export default function Comment ({author, body_html, id, depth, downs, ups, like
                     <div id={id}></div>
 
                     <div className="comment-interactions">
-                        <button onClick={e => {
-                            e.preventDefault();
-                            if (voting === "0" || voting === "-1") {
-                                vote(name, "1");
-                                setVoting("1");
-                                ups++;
-                            } else {
-                                vote(name, "0");
-                                setVoting("0");
-                                ups--;
-                            }
-                        }}>
-                            <img src={voting === "1" ? liked as unknown as string : like as unknown as string} alt="upvotes" decoding="sync"/><span>{formatAmount(ups)}</span>
-                        </button>
-                        
-                        <button onClick={e => {
-                            e.preventDefault();
-                            if (voting === "0" || voting === "1") {
-                                vote(name, "-1");
-                                setVoting("-1");
-                            } else {
-                                vote(name, "0");
-                                setVoting("0");
-                            }
-                        }}>
-                            <img src={voting === "-1" ? disliked as unknown as string : dislike as unknown as string} alt="downvotes" decoding="sync"/><span>{formatAmount(downs)}</span>
-                        </button>
-
-                        <button>
-                            <img src={comment_icon as unknown as string} alt="comments" decoding="sync"/> {formatAmount(replies)}
-                        </button>
-
-                        { replies !== 0 && t1 && 
-                            <a onClick={e => {
+                        <div className="interactions-left">
+                            <button onClick={e => {
                                 e.preventDefault();
-                                showReplies ? setShowReplies(false) : setShowReplies(true);
-                                }}><img src={showReplies ? minus_icon as unknown as string : plus_icon as unknown as string} 
-                                    alt="load replies" 
-                                    decoding="sync" 
-                                    style={{width: 20, height: 20}}/>
-                            </a>
+                                if (voting === "0" || voting === "-1") {
+                                    vote(name, "1");
+                                    setVoting("1");
+                                    ups++;
+                                } else {
+                                    vote(name, "0");
+                                    setVoting("0");
+                                    ups--;
+                                }
+                            }}>
+                                <img src={voting === "1" ? liked as unknown as string : like as unknown as string} alt="upvotes" decoding="sync"/><span>{formatAmount(ups)}</span>
+                            </button>
+                            
+                            <button onClick={e => {
+                                e.preventDefault();
+                                if (voting === "0" || voting === "1") {
+                                    vote(name, "-1");
+                                    setVoting("-1");
+                                } else {
+                                    vote(name, "0");
+                                    setVoting("0");
+                                }
+                            }}>
+                                <img src={voting === "-1" ? disliked as unknown as string : dislike as unknown as string} alt="downvotes" decoding="sync"/><span>{formatAmount(downs)}</span>
+                            </button>
+
+                            <button>
+                                <img src={comment_icon as unknown as string} alt="comments" decoding="sync"/> {formatAmount(replies)}
+                            </button>
+
+                            { replies !== 0 && t1 && t1[0] !== undefined && 
+                                <a onClick={e => {
+                                    e.preventDefault();
+                                    showReplies ? setShowReplies(false) : setShowReplies(true);
+                                    }}><img src={showReplies ? minus_icon as unknown as string : plus_icon as unknown as string} 
+                                        alt="load replies" 
+                                        decoding="sync" 
+                                        style={{width: 20, height: 20}}/>
+                                </a>
+                            }
+                        </div>
+                        {
+                            me === author ? 
+                            <div className="interactions-right">
+                                <button type="button" onClick={(e) => {
+                                    e.preventDefault();
+                                    del(name);
+                                    trigger ? setTrigger(false) : setTrigger(true);  
+                                }}><img src={bin} alt="delete comment" /></button>
+                            </div> : null
                         }
                     </div>
                 </div>
