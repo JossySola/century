e485ae157715a6f98df7d0a306f8cff31ead6d16
Jossy Solá <jossysola@gmail.com>
2024-07-18 00:19:30 -0600

    Passes likes prop to <Comment>

--- src/molecules/Replies/Replies.tsx
+++ src/molecules/Replies/Replies.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Comment from "../../atoms/Comment/Comment"
+import { getRepliesAmount } from "../Comments/Comments"
 import "./Replies.css"
  
 export default function Replies ({t1}) {
@@ -9,7 +10,6 @@ export default function Replies ({t1}) {
             <div className="replies-connector"></div>
             <div className="replies-comments">
                 {t1 && t1.map(comment => {
-                    
                     if (comment && comment.author !== "[deleted]") {
                         return <Comment 
                         key={comment.id}
@@ -18,9 +18,10 @@ export default function Replies ({t1}) {
                         body_html={comment.body_html}
                         depth={comment.depth} 
                         downs={comment.downs} 
+                        likes={comment.likes}
                         ups={comment.ups}
                         name={comment.name}
-                        replies={typeof comment.replies !== "string" && comment.replies.data.children.length > 0 ? comment.replies.data.children.length : 0}
+                        replies={getRepliesAmount(comment)}
                         more={comment.replies && comment.replies}/>
                     }
                 })}
