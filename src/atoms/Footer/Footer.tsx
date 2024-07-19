642c36c0bc880d3b412e3ec43546e99cb94eb4d7
Jossy Solá <jossysola@gmail.com>
2024-07-19 01:25:18 -0600

    Adds Privacy Policy and Terms of Use in footer

--- src/atoms/Footer/Footer.tsx
+++ src/atoms/Footer/Footer.tsx
@@ -35,6 +35,28 @@ export default function Footer () {
                 </div>
             </section>
 
+            <details className="footer-details">
+                <summary>Privacy Policy & Terms of Use</summary>
+                <br></br>
+                These Terms of Use and Privacy Policy (or "Terms") govern your use of this website, (hereafter, "Web Application" which refers to the URI: <a href="https://www.centurytimes.jossysola.com/">www.centurytimes.jossysola.com</a>). You (hereafter, "the User") agree with these Terms by accessing and using the Web Application's features.
+                <br></br>
+
+                <details>
+                    <summary>Privacy Policy</summary>
+                    <br></br>
+                    The collection and usage of data will always be for the Reddit platform communication purpose, if applicable.
+                    <br></br><br></br>
+                    No personal information is required by this Web Application, unless the User decides to write a comment in a post, which will direct them to the Reddit's Authorization page. The User may be asked for their Reddit username and password in order to give the Web Application permission to take actions (i.e. write and post a comment) in their behalf. However, any Authentication and/or Authorization process that requires sensitive data from the User will always take place on Reddit's official platform.
+                    <br></br><br></br>
+                    The Web Application does not store any data either externally (as it is not connected to any database) or locally. There may be interaction with the local cache by the Web Application's coding toolkit, targeting the performance of the Web Application and its functionality.
+                </details>
+                <details>
+                    <summary>Terms of Use</summary>
+                    <br></br>
+                    As the Web Application main feature is the display and usage of the Reddit Services (Reddit API, "Application Programming Interface", which establishes the interaction between Reddit and third party developers who wish to use Reddit's data/features in their own applications), the User of this Web Application is bound by the <a href="https://www.redditinc.com/policies/user-agreement" target="_blank">Reddit User Agreement</a>.
+                </details>
+            </details>
+
             <div className="footer-madein">
                 <p>Made with 💖 in Mexico</p>
                 <img src={mexico as SVG as string} alt="Mexico seal"></img>
