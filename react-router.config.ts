36189ecdeade027c1537beca5ff0aafdd78cd4be
Jossy Solá <jossysola@gmail.com>
2025-08-06 04:04:24 -0600

    Update react-router.config.ts

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,9 +1,11 @@
+import { vercelPreset } from '@vercel/react-router/vite';
 import type { Config } from "@react-router/dev/config";
 export default {
   appDirectory: "app",
   buildDirectory: "build",
   ssr: true,
   basename: "/",
+  presets: [vercelPreset()],
   future: {
     unstable_middleware: true,
   },
