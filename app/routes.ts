f751dac5955efa6b42e0061a123ae1048d9c34e2
Jossy Solá <jossysola@gmail.com>
2025-08-06 04:06:51 -0600

    Update routes.ts

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,9 @@
-import { 
+/*import { 
     type RouteConfig,
 } from "@react-router/dev/routes";
 import { flatRoutes } from "@react-router/fs-routes";
 
-export default flatRoutes() satisfies RouteConfig;
+export default flatRoutes() satisfies RouteConfig;*/
+import { type RouteConfig, index } from '@react-router/dev/routes';
+ 
+export default [index('routes/_index.tsx')] satisfies RouteConfig;
