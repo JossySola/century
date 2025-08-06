7f268b6fe633b3e11b7b0440b6322e2f8738df36
Jossy Solá <jossysola@gmail.com>
2025-08-05 23:41:37 -0600

    Creates config files

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { reactRouter } from "@react-router/dev/vite";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [
+    reactRouter(),
+    tailwindcss(),
+  ],
+});
