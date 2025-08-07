d1c973cf4ccb7d3edfc94403aad52612be0c3a8c
Jossy Solá <jossysola@gmail.com>
2025-08-07 13:08:26 -0600

    Removes fs-routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,12 +1,3 @@
-/*
-import { 
-    type RouteConfig,
-} from "@react-router/dev/routes";
-import { flatRoutes } from "@react-router/fs-routes";
-
-export default flatRoutes() satisfies RouteConfig;
-*/
-
 import { type RouteConfig, index } from '@react-router/dev/routes';
  
 export default [index('routes/index.tsx')] satisfies RouteConfig;
