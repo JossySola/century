89aef9c84433cff5824340401736a5bd3ba2bfeb
Jossy Solá <jossysola@gmail.com>
2025-08-08 14:42:26 -0600

    Create sessions.server.ts

---
+++ app/sessions.server.ts
@@ -0,0 +1,24 @@
+import { createCookieSessionStorage } from "react-router";
+
+type SessionData = {
+  access_token: string;
+};
+
+type SessionFlashData = {
+  error: string;
+};
+
+const { getSession, commitSession, destroySession } =
+  createCookieSessionStorage<SessionData, SessionFlashData>(
+    {
+      cookie: {
+        name: "__session",
+        httpOnly: true,
+        maxAge: 8400,
+        path: "/",
+        secure: true,
+      },
+    },
+  );
+
+export { getSession, commitSession, destroySession };
