58acae4899490fb8816ef8f629b64c9c98e0784b
Jossy Solá <jossysola@gmail.com>
2025-08-09 15:04:00 -0600

    Creates route

---
+++ app/routes/sports.tsx
@@ -0,0 +1,65 @@
+import { getSession } from "~/sessions.server";
+import type { Route } from "./+types";
+import { useEffect, useMemo, useState } from "react";
+import type { Thing } from "./index";
+import { useActionData, useSubmit } from "react-router";
+import T5 from "~/ui/cards/t5";
+
+export default function Main({ actionData }: Route.ComponentProps) {
+    const [data, setData] = useState<Array<Thing>>([]);
+    const submit = useSubmit();
+    const actionResult = useActionData<typeof action>();
+
+    useEffect(() => {
+        submit({}, { method: 'POST' })
+    }, [submit]);
+    useEffect(() => {
+        if (actionResult) {
+            setData(actionResult);
+        }
+    }, [actionResult]);
+
+    const posts = useMemo(() => data && data.map((element: Thing, index: number) => {
+        return <T5 
+        key={ index }
+        author={ element.data.author }
+        id={ element.data.id }
+        permalink={ element.data.permalink }
+        num_comments={ element.data.num_comments }
+        selftext={ element.data.selftext }
+        subreddit={ element.data.subreddit }
+        subreddit_id={ element.data.subreddit_id }
+        thumbnail={ element.data.thumbnail }
+        thumbnail_height={ element.data.thumbnail_height }
+        thumbnail_width={ element.data.thumbnail_width }
+        title={ element.data.title }
+        ups={ element.data.ups }/>
+    }), [data]);
+    return (
+        posts
+    )
+}
+
+export async function action({ request }: Route.ActionArgs) {
+    const session = await getSession(
+        request.headers.get("Cookie"),
+    );
+    const tokenCookie = session.get("access_token");
+    
+    const req = await fetch("https://www.reddit.com/r/sports.json?raw_json=1", {
+        method: "GET",
+        headers: {
+            "Authorization": `${tokenCookie}`,
+        },
+    });
+    if (req.status !== 200) {
+        console.error(req.statusText);
+        console.error(req.status)
+        throw new Error("Failed at fetching subreddits");
+    }
+    const response = await req.json();
+    return response.data.children;
+}
+export function HydrateFallback() {
+    return <p>Loading...</p>
+}
