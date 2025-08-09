f340bbb8194f709b11493d3de9b7e0862f2815d0
Jossy Solá <jossysola@gmail.com>
2025-08-09 15:08:00 -0600

    Sets Link attributes

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -1,6 +1,9 @@
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenu, NavbarMenuItem, NavbarMenuToggle } from "@heroui/react";
 import { useState } from "react";
 import { Link } from "react-router";
+import { GamesTopic, SciencesTopic, SpookyTopic, SportsTopic, TechnologyTopic } from "./icons";
+import Logo from "/Reddit_Logo_Wordmark_OrangeRed.svg";
+import Search from "./inputs/search";
 
 export default function HeaderMenu() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -10,24 +13,33 @@ export default function HeaderMenu() {
                 <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} className="sm:hidden" />
             </NavbarContent>
             <NavbarContent justify="center">
-                <NavbarBrand><h1 className="title text-[8vw] sm:text-[2.25rem]">The 21st Century Times</h1></NavbarBrand>
+                <NavbarBrand>
+                    <Link to="/">
+                        <h1 className="title text-[8vw] sm:text-[2.25rem]">The 21st Century Times</h1>
+                    </Link>
+                </NavbarBrand>
             </NavbarContent>
-            <NavbarMenu>
+            <NavbarMenu className="pt-10 gap-3">
+                <Search />
                 <NavbarMenuItem>
-                    <Link to="/astronomy" className="w-full font-semibold">Astronomy</Link>
+                    <Link to="/space" prefetch="viewport" className="w-full font-semibold text-2xl inline-flex items-center gap-3"><SpookyTopic/> Space</Link>
                 </NavbarMenuItem>
                 <NavbarItem>
-                    <Link to="/gaming" className="w-full font-semibold">Gaming</Link>
+                    <Link to="/gaming" prefetch="viewport" className="w-full font-semibold text-2xl inline-flex items-center gap-3"><GamesTopic /> Gaming</Link>
                 </NavbarItem>
                 <NavbarItem>
-                    <Link to="/science" className="w-full font-semibold">Science</Link>
+                    <Link to="/science" prefetch="viewport" className="w-full font-semibold text-2xl inline-flex items-center gap-3"><SciencesTopic /> Science</Link>
                 </NavbarItem>
                 <NavbarItem>
-                    <Link to="/sports" className="w-full font-semibold">Sports</Link>
+                    <Link to="/sports" prefetch="viewport" className="w-full font-semibold text-2xl inline-flex items-center gap-3"><SportsTopic /> Sports</Link>
                 </NavbarItem>
                 <NavbarItem>
-                    <Link to="/tech" className="w-full font-semibold">Tech</Link>
+                    <Link to="/tech" prefetch="viewport" className="w-full font-semibold text-2xl inline-flex items-center gap-3"><TechnologyTopic /> Tech</Link>
                 </NavbarItem>
+                <div className="w-full mt-5 inline-flex items-center gap-3">
+                    <span className="text-xl">Powered with </span>
+                    <img src={Logo} width={64} alt="Reddit Wordmark" />
+                </div>
             </NavbarMenu>
         </Navbar>
     )
