078abfafee3931c1cc4c806613be65956be5b2b7
Jossy Solá <jossysola@gmail.com>
2025-08-09 16:51:35 -0600

    Changes index route name

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,7 @@
 import { type RouteConfig, index, route } from '@react-router/dev/routes';
  
 export default [
-    index('./routes/index.tsx'),
+    index('./routes/news.tsx'),
     route('space', './routes/space.tsx'),
     route('gaming', './routes/gaming.tsx'),
     route('science', './routes/science.tsx'),
