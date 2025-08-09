170da17c97822d352a60228449c7ea2ec24eb7a9
Jossy Solá <jossysola@gmail.com>
2025-08-09 00:09:27 -0600

    Sets styling

--- app/ui/inputs/search.tsx
+++ app/ui/inputs/search.tsx
@@ -3,7 +3,18 @@ import { MagnifyingGlass } from "../icons";
 
 export default function Search() {
     return <form className="w-full flex flex-row gap-3 justify-center items-center">
-        <Input label="Search on Reddit" variant="bordered" endContent={ <MagnifyingGlass /> } />
+        <Input 
+        label="Search on Reddit" 
+        variant="underlined" 
+        endContent={ 
+            <div className="h-full inline-flex justify-center items-center">
+                <MagnifyingGlass />
+            </div>
+        }
+        classNames={{
+            label: "text-sm font-sans",
+            input: "h-full"
+        }} />
         <Button color="primary"><span className="text-lg">Go</span></Button>
     </form>
 }
