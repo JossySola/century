2edc325d68985575d7b796fc70026039d3b3d5c5
Jossy Solá <jossysola@gmail.com>
2025-08-11 23:07:35 -0600

    Sets structure

--- app/ui/drawers/comments.tsx
+++ app/ui/drawers/comments.tsx
@@ -1,21 +1,28 @@
 import { Button, Drawer, DrawerBody, DrawerContent, DrawerFooter, DrawerHeader, useDisclosure } from "@heroui/react";
+import type { Listing, Thing } from "~/utils/types";
 
-export default function Comments({ num_comments }: {
+export default function Comments({ num_comments, comments }: {
     num_comments: number,
+    comments?: Listing,
 }) {
     const {isOpen, onOpen, onOpenChange} = useDisclosure();
-
     return (
         <>
-        <Button onPress={onOpen} isDisabled={ num_comments === 0 } size="lg" className="w-full" color="danger"><span className="text-lg">Read comments</span></Button>
+        <Button onPress={onOpen} isDisabled={ num_comments === 0 } size="lg" className="w-full p-2" color="danger"><span className="text-lg">Read comments</span></Button>
         <Drawer isOpen={isOpen} onOpenChange={onOpenChange} placement="bottom" size="lg">
             <DrawerContent>
                 {
                     onClose => (
                         <>
                         <DrawerHeader></DrawerHeader>
-                        <DrawerBody></DrawerBody>
-                        <DrawerFooter></DrawerFooter>
+                        <DrawerBody>
+                            <section>
+
+                            </section>
+                        </DrawerBody>
+                        <DrawerFooter>
+                            <Button onPress={onClose}><span>Close</span></Button>
+                        </DrawerFooter>
                         </>
                     )
                 }
