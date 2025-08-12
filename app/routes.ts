57fc2271bac010faf51ff7cad2635e94d918836f
Jossy Solá <jossysola@gmail.com>
2025-08-11 23:09:19 -0600

    Adds catch-all route, and API endpoint

--- app/routes.ts
+++ app/routes.ts
@@ -2,12 +2,14 @@ import { type RouteConfig, index, prefix, route } from '@react-router/dev/routes
  
 export default [
     index('./routes/news.tsx'),
+    route("*", "./routes/$.tsx"),
     route('space', './routes/space.tsx'),
     route('gaming', './routes/gaming.tsx'),
     route('science', './routes/science.tsx'),
     route('sports', './routes/sports.tsx'),
     route('tech', './routes/tech.tsx'),
     ...prefix("api", [
-        route("subreddit/*", "./api/subreddit.tsx"),
+        route("subreddit/*", "./api/subreddit.$.tsx"),
+        route("author/:name", "./api/author.$name.tsx"),
     ]),
 ] satisfies RouteConfig;
