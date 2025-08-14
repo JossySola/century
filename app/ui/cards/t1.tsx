ea43973fc10276ddaeb81c1092ee219a801d3eb5
Jossy Solá <jossysola@gmail.com>
2025-08-13 20:00:52 -0600

    Sets useEffect hooks to fetch Reddit endpoint.

--- app/ui/cards/t1.tsx
+++ app/ui/cards/t1.tsx
@@ -1,28 +1,53 @@
-import { Card, CardBody } from "@heroui/react";
-import { useEffect, useState } from "react";
+import { Avatar, Card, CardBody } from "@heroui/react";
+import { memo, useEffect, useState } from "react";
 import { useFetcher } from "react-router";
-import type { Thing } from "~/utils/types";
+import type { T1 as T1type, T2 } from "~/utils/types";
+import { Heart, Message } from "../icons";
 
-export default function T1({ comment }: { comment: Thing }) {
+const avatarCache: Record<string, string> = {};
+const T1 = memo(({ comment, isOpen }: { comment: T1type, isOpen: boolean }) => {
     const [image, setImage] = useState<string>("");
     const fetcher = useFetcher();
     useEffect(() => {
-        if (comment.kind === "t1") {
-            if (comment.data.author) {
-                fetcher.load(`/api/author/${comment.data.author}`);
-            }
+        if (!isOpen) return;
+        if (comment.kind !== "t1") return;
+        const author = comment.data.author;
+        if (!author) return;
+        if (avatarCache[author]) {
+            setImage(avatarCache[author]);
+            return;
         }
-    }, []);
+        fetcher.load(`/api/author/${author}`);
+    }, [isOpen, comment.data.author, comment.kind]);
     useEffect(() => {
         if (fetcher.data) {
-
+            const data: T2 = fetcher.data;
+            const author = comment.data.author;
+            const url = data.data.snoovatar_img;
+            if (author && url) {
+                avatarCache[author] = url;
+                setImage(url);
+            }
         }
-    }, [fetcher.data]);
+    }, [fetcher.data, comment.data.author]);
     return (
-        <Card>
+        <Card className="w-full">
             <CardBody>
-
+                <div className="grid grid-flow-row grid-rows-[auto_auto_auto] grid-cols-1 gap-3">
+                    <div className="col-span-1 row-start-1 row-span-1 flex flex-row gap-3">
+                        <Avatar size="sm" src={image ?? undefined}/>
+                        <span className="font-semibold">{ comment.data.author }</span>
+                    </div>
+                    <div className="col-span-1 row-start-2 row-span-1 flex flex-row gap-3">
+                        <p className="font-['Arial']">{ comment.data.body }</p>
+                    </div>
+                    <div className="col-span-1 row-start-3 row-span-1 flex flex-row gap-3">
+                        <span className="inline-flex items-center justify-center gap-3"><Heart /> { comment.data.ups }</span>
+                        <span className="inline-flex items-center justify-center gap-3"><Message /> { comment.data.replies ? comment.data.replies.data.children.length : 0 }</span>
+                    </div>
+                </div>
             </CardBody>
         </Card>
     )
-}
+})
+export default T1;
