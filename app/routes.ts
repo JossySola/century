466c420ff18e0ca21f2247eca485f3d296037c55
Jossy Solá <jossysola@gmail.com>
2025-08-14 18:48:29 -0600

    Creates routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,13 +1,15 @@
 import { type RouteConfig, index, prefix, route } from '@react-router/dev/routes';
  
 export default [
-    index('./routes/news.tsx'),
+    index('./routes/_index.tsx'),
     route("*", "./routes/$.tsx"),
     route('space', './routes/space.tsx'),
     route('gaming', './routes/gaming.tsx'),
     route('science', './routes/science.tsx'),
     route('sports', './routes/sports.tsx'),
     route('tech', './routes/tech.tsx'),
+    route('news', './routes/news.tsx'),
+    route('r/:subreddit', "./routes/r.$subreddit.tsx"),
     ...prefix("api", [
         route("subreddit/*", "./api/subreddit.$.tsx"),
         route("author/:name", "./api/author.$name.tsx"),
