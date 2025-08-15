07d3b39d5d597d52c16aa761915366a4f4e1a4c9
Jossy Solá <jossysola@gmail.com>
2025-08-14 18:47:27 -0600

    Uses useState to store input's value. Uses Form from react-router.

--- app/ui/inputs/search.tsx
+++ app/ui/inputs/search.tsx
@@ -1,12 +1,17 @@
 import { Button, Input } from "@heroui/react";
-import { MagnifyingGlass } from "../icons";
 import Logo from "/Reddit_Icon_FullColor.svg";
+import { useState } from "react";
+import { Form } from "react-router";
 
 export default function Search() {
-    return <form method="post" className="w-full flex flex-row gap-3 justify-center items-center">
+    const [value, setValue] = useState<string>("");
+    return <Form action="/" method="post" className="w-full flex flex-row gap-3 justify-center items-center">
         <Input 
         label="Search on Reddit" 
+        name="query"
         variant="underlined" 
+        value={value}
+        onValueChange={setValue}
         endContent={ 
             <div className="h-full inline-flex justify-center items-center">
                 <img src={Logo} width={32} alt="Reddit bubble icon" />
@@ -14,8 +19,8 @@ export default function Search() {
         }
         classNames={{
             label: "text-sm font-sans",
-            input: "h-full"
+            input: "h-full font-['Arial']"
         }} />
         <Button color="primary" type="submit" size="sm"><span className="text-lg">Go</span></Button>
-    </form>
+    </Form>
 }
