9630bad29450b4d616f7f7d72bed3639d3dedf44
Jossy Solá <jossysola@gmail.com>
2025-08-18 02:01:53 -0600

    Starts implementing infinite scroll

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,11 @@
 import { getSession } from "~/sessions.server";
 import type { Route } from "./+types/_index";
-import { useMemo } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import T3 from "~/ui/cards/t3";
 import type { Listing, Thing } from "~/utils/types";
 import { useOutletContext } from "react-router";
 import T5 from "~/ui/cards/t5";
+import { Spinner } from "@heroui/react";
 
 export async function loader({ request }: Route.LoaderArgs) {
     const session = await getSession(
@@ -30,7 +31,102 @@ export async function loader({ request }: Route.LoaderArgs) {
 }
 export default function Index({ loaderData }: Route.ComponentProps) {
     const action: Listing = useOutletContext();
-    const objectsFromLoader = useMemo(() => loaderData.map((element: Thing, index: number) => {
+    const scrollableRef = useRef<HTMLDivElement>(null);
+    const scrollPositionRef = useRef(0);
+    const loadingRef = useRef(null);
+    const [feed , setFeed] = useState<Array<Thing>>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    useEffect(() => {
+        if (feed.length > 0) return;
+        addToFeed();
+    }, []);
+    useEffect(() => {
+        if (!scrollableRef.current || !loadingRef.current) return;
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                if (entry.isIntersecting) {
+                    addToFeed();
+                }
+            }, 
+            {
+                root: scrollableRef.current,
+                threshold: 1,
+            }
+        );
+        if (loadingRef.current) {
+            observer.observe(loadingRef.current);
+        }
+        return () => observer.disconnect();
+    }, [feed]);
+    useEffect(() => {
+        if (!isLoading) {
+            requestAnimationFrame(() => {
+                restoreScrollPosition();
+            })
+        }
+    }, [feed, isLoading]);
+
+    const addToFeed = () => {
+        setIsLoading(true);
+        saveScrollPosition();
+        if (loaderData) {
+            setFeed(prev => {
+                const diff = loaderData.length - prev.length;
+                if ((loaderData.length && loaderData.length <= 5) || diff < 5) {
+                    return loaderData;
+                };
+                const count = prev.length + 5;
+                const newArray = loaderData.slice(0, count);
+                /*
+                if (loaderData.length !== feed.length || (action && action.data && action.data.children).length !== feed.length) {
+                    newArray.push(<div ref={loadingRef} className="flex flex-row justify-center items-center w-full h-fit mb-10">
+                        <Spinner variant="wave" color="primary" size="lg" />
+                    </div>);
+                    return newArray;
+                } 
+                */
+                return newArray;
+            });
+            return;
+        };
+        if (action && action.data && action.data.children) {
+            setFeed(prev => {
+                const children = action.data.children;
+                const diff = children.length - prev.length;
+
+                if ((children.length && children.length <= 5) || diff < 5) {
+                    return children;
+                };
+                const count = prev.length + 5;
+                const newArray = loaderData.slice(0, count);
+                /*
+                if (loaderData.length !== feed.length || (action && action.data && action.data.children).length !== feed.length) {
+                    newArray.push(<div ref={loadingRef} className="flex flex-row justify-center items-center w-full h-fit mb-10">
+                        <Spinner variant="wave" color="primary" size="lg" />
+                    </div>);
+                    return newArray;
+                }
+                */
+                return newArray;
+            });
+            return;
+        };
+        setIsLoading(false);
+        return;
+    }
+    const saveScrollPosition = () => {
+        if (scrollableRef.current) {
+            scrollPositionRef.current = scrollableRef.current.scrollTop;
+        }
+    };
+    const restoreScrollPosition = () => {
+        if (scrollableRef.current) {
+            scrollableRef.current.scrollTop = scrollPositionRef.current;
+        }
+    };
+
+    const render = feed.map((element: Thing, index: number) => {
         if (element.kind === "t3") {
             return <T3 
             key={ index }
@@ -47,9 +143,6 @@ export default function Index({ loaderData }: Route.ComponentProps) {
             title={ element.data.title ?? "" }
             ups={ element.data.ups } />
         }
-    }), [loaderData]);
-    
-    const objectsFromAction = useMemo(() => action?.data.children.map((element: Thing, index: number) => {
         if (element.kind === "t5") {
             return <T5 
             key={index}
@@ -60,14 +153,12 @@ export default function Index({ loaderData }: Route.ComponentProps) {
             banner_img={element.data.banner_img}
             icon_img={element.data.icon_img}
             />
-        }
-    }), [action]);
+        }          
+    });
     
     return (
-        <>
-        {
-            objectsFromAction ?? objectsFromLoader
-        }
-        </>
+        <section ref={scrollableRef} className="flex flex-col items-center gap-5">
+            { render }
+        </section>
     )
 }
