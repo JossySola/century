3dcae6b02b2b1f5561522e621d851891033d1e34
Jossy Solá <jossysola@gmail.com>
2025-08-18 02:04:05 -0600

    Adds .replace to url and conditionally renders <CardBody>

--- app/ui/cards/t3.tsx
+++ app/ui/cards/t3.tsx
@@ -25,19 +25,22 @@ export default function T3({ author, subreddit, id, permalink, num_comments, sel
     const [moreData, setMoreData] = useState<Array<Listing>>([]);
     const fetcher = useFetcher();
     const [preview, setPreview] = useState<string | undefined>(undefined);
-    // Make a useEffect fetch when isOpen is true, to gather further data from subreddit
+    // If there is no thumbnail passed in the props or there is a preview set, it returns nothing.
+    // Otherwise, it fetches from the endpoint
     useEffect(() => {
-        if (!isOpen) return;
         if (!thumbnail) return;
         if (preview) return;
         fetcher.load(`/api/subreddit/${permalink}`);
-    }, [isOpen, thumbnail, preview, permalink]);
+    }, [thumbnail, preview, permalink]);
+    // This is triggered when there is change on fetcher.data
+    // It sets fetcher.data into setMoreData state.
     useEffect(() => {
         if (fetcher.data) {
             const data: Array<Listing> = fetcher.data;
             setMoreData(data);
         }
     }, [fetcher.data]);
+    // When moreData state changes, it looks for the preview image and sets it into the Preview state.
     useEffect(() => {
         if (moreData && moreData[0] && moreData[0].kind === "Listing") {
             const children = moreData[0].data.children;
@@ -45,7 +48,7 @@ export default function T3({ author, subreddit, id, permalink, num_comments, sel
                 if (children[0].kind === "t3" && children[0].data.preview) {
                     const preview = children[0].data.preview;
                     if (preview.images.length > 0) {
-                        const url = preview.images[0].source.url;
+                        const url = preview.images[0].source.url.replace(/&amp;/g, "&");
                         setPreview(url);
                     }
                 }
@@ -54,20 +57,31 @@ export default function T3({ author, subreddit, id, permalink, num_comments, sel
     }, [moreData]);
     return (
         <>
-        <motion.button initial={{ scale: 0.5 }} animate={{ scale: 1 }} onClick={() => onOpen()} className="cursor-pointer w-full max-w-[90vw] md:w-[532px]">
+        <motion.button initial={{ scale: 0.5, opacity: 0 }} animate={{ scale: 1, opacity: 1 }} onClick={() => onOpen()} className="cursor-pointer w-full max-w-[90vw] md:w-[532px]">
             <Card className="p-5">
                 <CardHeader className="flex flex-col gap-3 text-center">
                     <User name={author} description={subreddit} />
                     <h4 className="w-full">{title}</h4>
                 </CardHeader>
-                <CardBody className="flex flex-col justify-center items-center text-center">
-                    {
-                        thumbnail 
-                        ? <Image src={thumbnail} width={thumbnail_width} />
-                        : null 
-                    }
-                    <p className="w-full line-clamp-3 overflow-hidden text-ellipsis">{selftext}</p>
-                </CardBody>
+                {
+                    preview || selftext
+                    ?  <CardBody className="h-fit flex flex-col justify-center items-center text-center">
+                        {
+                            preview 
+                            ? <Image src={preview} alt="cover image" classNames={{
+                                    img: "h-fit"
+                            }} />
+                            : null
+                        }
+                        {
+                            !preview && thumbnail 
+                            ? <Image src={thumbnail} width={thumbnail_width} alt="cover image" /> 
+                            : null
+                        }
+                        <p className="w-full line-clamp-3 overflow-hidden text-ellipsis">{selftext}</p>
+                    </CardBody>
+                    : null
+                }
                 <Divider />
                 <CardFooter className="flex flex-row text-gray-500">
                     <div className="flex flex-row justify-center items-center gap-3">
@@ -99,12 +113,18 @@ export default function T3({ author, subreddit, id, permalink, num_comments, sel
                     </ModalHeader>
                     <ModalBody>
                         <div className="inline-flex items-center justify-center">
-                            {
-                                preview && <motion.div initial={{ scale: 0.5, opacity: 0 }} animate={{ scale: 1, opacity: 1 }}><Image src={preview} alt="cover image" /></motion.div>
-                            }
-                            {
-                                !preview && thumbnail ? <Image src={thumbnail} width={thumbnail_width} alt="cover image" /> : null
-                            }
+                        {
+                            preview 
+                            ? <Image src={preview} alt="cover image" classNames={{
+                                    img: "h-fit"
+                            }} />
+                            : null
+                        }
+                        {
+                            !preview && thumbnail 
+                            ? <Image src={thumbnail} width={thumbnail_width} alt="cover image" /> 
+                            : null
+                        }
                         </div>
                         
                         <p className="w-full overflow-clip p-x-3">{selftext}</p>
