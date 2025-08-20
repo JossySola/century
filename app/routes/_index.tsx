f456f8483261abc001e9dab08cccce219fead15a
Jossy Solá <jossysola@gmail.com>
2025-08-20 15:35:22 -0600

    Implements useInfiniteScroll

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,11 +1,6 @@
 import { getSession } from "~/sessions.server";
 import type { Route } from "./+types/_index";
-import { useEffect, useMemo, useRef, useState } from "react";
-import T3 from "~/ui/cards/t3";
-import type { Listing, Thing } from "~/utils/types";
-import { useOutletContext } from "react-router";
-import T5 from "~/ui/cards/t5";
-import { Spinner } from "@heroui/react";
+import useInfiniteScroll from "~/utils/custom-hooks";
 
 export async function loader({ request }: Route.LoaderArgs) {
     const session = await getSession(
@@ -30,155 +25,12 @@ export async function loader({ request }: Route.LoaderArgs) {
     return response.data.children;
 }
 export default function Index({ loaderData }: Route.ComponentProps) {
-    const action: Listing = useOutletContext();
-    const scrollPositionRef = useRef(0);
-    const loadingRef = useRef(null);
-    const [feed , setFeed] = useState<Array<Thing>>([]);
-    const [isLoading, setIsLoading] = useState<boolean>(false);
-
-    useEffect(() => {
-        if (feed.length > 0) return;
-        addToFeed();
-    }, []);
-    useEffect(() => {
-        if (!loadingRef.current) return;
-        const observer = new IntersectionObserver(
-            ([entry]) => {
-                if (entry.isIntersecting) {
-                    addToFeed();
-                }
-            }, 
-            {
-                root: document,
-                threshold: 1,
-            }
-        );
-        if (loadingRef.current) {
-            observer.observe(loadingRef.current);
-        }
-        return () => observer.disconnect();
-    }, [feed.length]);
-    useEffect(() => {
-        if (!isLoading) {
-            requestAnimationFrame(() => {
-                restoreScrollPosition();
-            })
-        }
-    }, [feed, isLoading]);
-
-    const addToFeed = () => {
-        setIsLoading(true);
-        saveScrollPosition();
-        if (loaderData) {
-            setFeed(prev => {
-                if (prev.length === loaderData.length) {
-                    return prev;
-                }
-                if (loaderData.length <= 5) {
-                    return loaderData;
-                }
-                const diff = loaderData.length - prev.length;
-                if (diff < 5) {
-                    return loaderData;
-                };
-
-                const count = prev.length + 5;
-                const newArray = loaderData.slice(0, count);
-                console.log("LOADERDATA: ", loaderData.length)
-                console.log("FEED: ", newArray.length)
-                return newArray;
-            });
-            return;
-        };
-        if (action && action.data && action.data.children) {
-            setFeed(prev => {
-                const children = action.data.children;
-                if (prev.length === children.length) {
-                    return prev;
-                }
-                if (children.length <= 5) {
-                    return children;
-                }
-                const diff = children.length - prev.length;
-                if (diff < 5) {
-                    return children;
-                };
-                const count = prev.length + 5;
-                const newArray = children.slice(0, count);
-                console.log("CHILDREN: ", children.length)
-                console.log("FEED: ", newArray.length)
-                return newArray;
-            });
-            return;
-        };
-        setIsLoading(false);
-        return;
-    }
-    const saveScrollPosition = () => {
-        const scrollable = document.documentElement ?? document.body;
-        if (scrollable) {
-            scrollPositionRef.current = scrollable.scrollTop;
-        }
-    };
-    const restoreScrollPosition = () => {
-        const scrollable = document.documentElement ?? document.body;
-        if (scrollable) {
-            scrollable.scrollTop = scrollPositionRef.current;
-        }
-    };
-    const renderLoadingDots = () => {
-        if (loaderData) {
-            if (loaderData.length !== feed.length) {
-                return <div ref={loadingRef} className="relative bottom-0 flex flex-row justify-center w-full h-fit p-y-10">
-                    <Spinner variant="wave" color="primary" size="lg" />
-                </div>
-            }
-        } else if (action && action.data) {
-            if (action.data.children && action.data.children.length !== feed.length) {
-                return <div ref={loadingRef} className="relative bottom-0 flex flex-row justify-center w-full h-fit p-y-10">
-                    <Spinner variant="wave" color="primary" size="lg" />
-                </div>
-            }
-        }
-        return null;
-    }
-
-    const render = feed.map((element: Thing, index: number) => {
-        if (element.kind === "t3") {
-            return <T3 
-            key={ index }
-            author={ element.data.author }
-            id={ element.data.id }
-            permalink={ element.data.permalink }
-            num_comments={ element.data.num_comments ?? 0 }
-            selftext={ element.data.selftext ?? "" }
-            subreddit={ element.data.subreddit ?? "" }
-            subreddit_id={ element.data.subreddit_id }
-            thumbnail={ element.data.thumbnail ?? "" }
-            thumbnail_height={ element.data.thumbnail_height ?? 0 }
-            thumbnail_width={ element.data.thumbnail_width ?? 0 }
-            title={ element.data.title ?? "" }
-            ups={ element.data.ups } />
-        }
-        if (element.kind === "t5") {
-            return <T5 
-            key={index}
-            display_name_prefixed={element.data.display_name_prefixed}
-            subscribers={element.data.subscribers}
-            name={element.data.name}
-            public_description={element.data.public_description}
-            banner_img={element.data.banner_img}
-            icon_img={element.data.icon_img}
-            />
-        }          
-    });
+    const { render, renderLoadingDots } = useInfiniteScroll(loaderData);
     
     return (
         <section className="flex flex-col items-center gap-5 w-full mb-5">
             { render }
-            {
-                renderLoadingDots()
-            }
+            { renderLoadingDots() }
         </section>
     )
 }
--- app/routes/gaming.tsx
+++ app/routes/gaming.tsx
@@ -1,56 +1,9 @@
 import { getSession } from "~/sessions.server";
 import type { Route } from "./+types/gaming";
-import { useEffect, useMemo, useState } from "react";
-import type { Thing } from "~/utils/types";
-import { useActionData, useSubmit } from "react-router";
-import T3 from "~/ui/cards/t3";
 import { Spinner } from "@heroui/react";
+import useInfiniteScroll from "~/utils/custom-hooks";
 
-export default function Main({ actionData }: Route.ComponentProps) {
-    const [data, setData] = useState<Array<Thing>>([]);
-    const submit = useSubmit();
-    const actionResult = useActionData<typeof action>();
-
-    useEffect(() => {
-        submit({}, { method: 'POST' })
-    }, [submit]);
-    useEffect(() => {
-        if (actionResult) {
-            console.log(actionResult)
-            setData(actionResult);
-        }
-    }, [actionResult]);
-
-    const posts = useMemo(() => data && data.map((element: Thing, index: number) => {
-        if (element.kind === "t3") {
-            return <T3 
-                key={ index }
-                author={ element.data.author }
-                id={ element.data.id }
-                permalink={ element.data.permalink }
-                num_comments={ element.data.num_comments ?? 0 }
-                selftext={ element.data.selftext ?? "" }
-                subreddit={ element.data.subreddit ?? "" }
-                subreddit_id={ element.data.subreddit_id }
-                thumbnail={ element.data.thumbnail ?? "" }
-                thumbnail_height={ element.data.thumbnail_height ?? 0 }
-                thumbnail_width={ element.data.thumbnail_width ?? 0 }
-                title={ element.data.title ?? "" }
-                ups={ element.data.ups }/>
-        }
-    }), [data]);
-    
-    return (
-        <section className="flex flex-col items-center gap-3 w-full">
-            <h3>Gaming</h3>
-            {
-                posts
-            }
-        </section>
-    )
-}
-
-export async function action({ request }: Route.ActionArgs) {
+export async function loader({ request }: Route.ActionArgs) {
     const session = await getSession(
         request.headers.get("Cookie"),
     );
@@ -71,6 +24,17 @@ export async function action({ request }: Route.ActionArgs) {
     }
     const response = await req.json();
     return response.data.children;
+}
+export default function Main({ loaderData }: Route.ComponentProps) {
+    const { render, renderLoadingDots } = useInfiniteScroll(loaderData);
+    
+    return (
+        <section className="flex flex-col items-center gap-3 w-full">
+            <h3 className="reddit-header my-5">Gaming</h3>
+            { render }
+            { renderLoadingDots() }
+        </section>
+    )
 }
 export function HydrateFallback() {
     return <Spinner size="lg" color="primary" label="Loading..." />
