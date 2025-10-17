48c206e9984c670e02f63890ed4b87a58b5bcecc
Jossy Solá <jossysola@gmail.com>
2025-10-16 23:00:50 -0600

    Implements clientLoader

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,8 @@
 import { getSession } from "~/sessions.server";
 import type { Route } from "./+types/_index";
 import useInfiniteScroll from "~/utils/custom-hooks";
+import { useEffect } from "react";
+import { addToast } from "@heroui/react";
 
 export async function loader({ request }: Route.LoaderArgs) {
     const session = await getSession(
@@ -22,11 +24,38 @@ export async function loader({ request }: Route.LoaderArgs) {
         throw new Error("Failed at fetching subreddits");
     }
     const response = await req.json();
-    return response.data.children;
+    return {
+        data: response.data.children,
+        url: request.url,
+    };
+}
+export async function clientLoader({
+  params,
+}: Route.ClientLoaderArgs) {
+    const pendingAction = window.sessionStorage.getItem("x-century-pending-action");
+    const pendingUrl = window.sessionStorage.getItem("x-century-pending-url");
+
+    if (pendingAction && pendingUrl) {
+        const parsedAction = JSON.parse(pendingAction);
+        window.sessionStorage.removeItem("x-century-pending-action");
+        window.sessionStorage.removeItem("x-century-pending-url");
+        console.log(parsedAction, pendingUrl);
+    }
 }
 export default function Index({ loaderData }: Route.ComponentProps) {
-    const { render, renderLoadingDots } = useInfiniteScroll(loaderData);
-    
+    const { render, renderLoadingDots } = useInfiniteScroll(loaderData.data);
+    //https://www.centurytimes.jossysola.com/?state=x&error=access_denied#_ 
+    useEffect(() => {
+        const url = new URL(loaderData.url);
+        const error = url.searchParams.get("error");
+        if (error) {
+            addToast({ 
+                title: "Authorization Error", 
+                description: "You need to authorize the app to upvote and comment.",
+                color: "danger", 
+            });
+        }
+    }, []);
     return (
         <section className="flex flex-col items-center gap-5 w-full mb-5">
             { render }
