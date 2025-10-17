8f9990cd66e91aaa5ea949dd5a49cbbde772b698
Jossy Solá <jossysola@gmail.com>
2025-10-16 23:13:36 -0600

    Creates function

---
+++ app/utils/get-category-content.ts
@@ -0,0 +1,17 @@
+export default async function getCategoryContent(category: string, cookie: string | undefined) {
+    const req = await fetch(`https://www.reddit.com/r/${category}.json?raw_json=1`, {
+        method: "GET",
+        headers: {
+            'Authorization': `Basic ${cookie}`,
+            'Content-Type': 'application/json',
+            'User-Agent': "centurytimes/2.0",
+        },
+    });
+    if (req.status !== 200) {
+        console.error(req.statusText);
+        console.error(req.status)
+        throw new Error("Failed at fetching subreddits");
+    }
+    const response = await req.json();
+    return response.data.children;
+}
