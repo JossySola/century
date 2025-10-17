3d98f8ee510d6c7a35d611c5cfa0c4431d08c81d
Jossy Solá <jossysola@gmail.com>
2025-10-16 22:59:31 -0600

    Uses sessionStorage to store the pending action before Reddit Authorization

--- app/ui/buttons/heart.tsx
+++ app/ui/buttons/heart.tsx
@@ -1,5 +1,5 @@
 import { useEffect, type Dispatch, type SetStateAction } from "react";
-import { useFetcher } from "react-router";
+import { useFetcher, useParams } from "react-router";
 import { Heart, HeartFill } from "../icons";
 import { motion } from "motion/react";
 
@@ -8,11 +8,17 @@ export default function HeartButton({ vote, setVote, id }: {
     setVote: Dispatch<SetStateAction<string>>, 
     id: string 
 }) {
+    const params = useParams();
     const fetcher = useFetcher();
-    useEffect(() => {      
-        //https://www.centurytimes.jossysola.com/?state=x&error=access_denied#_ 
+    useEffect(() => {
         if (fetcher.data) {
             if (fetcher.data.endpoint) {
+                window.sessionStorage.setItem("x-century-pending-action", JSON.stringify({
+                    type: "vote",
+                    id,
+                    vote,
+                }));
+                window.sessionStorage.setItem("x-century-pending-url", JSON.stringify(params));
                 window.location.href = fetcher.data.endpoint;
             } else if (fetcher.data.error) {
                 console.error("Error processing vote:", fetcher.data.error);
