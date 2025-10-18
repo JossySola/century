a6bd2bc2759213e392f700b7292336682fdaf227
Jossy Solá <jossysola@gmail.com>
2025-10-18 00:17:46 -0600

    Implements clientLoader and HydrateFallback

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,7 +2,7 @@ import { getSession } from "~/sessions.server";
 import type { Route } from "./+types/_index";
 import useInfiniteScroll from "~/utils/custom-hooks";
 import { useEffect } from "react";
-import { addToast } from "@heroui/react";
+import { addToast, Spinner } from "@heroui/react";
 import getCategoryContent from "~/utils/get-category-content";
 
 export async function loader({ request }: Route.LoaderArgs) {
@@ -16,18 +16,50 @@ export async function loader({ request }: Route.LoaderArgs) {
         url: request.url,
     };
 }
-export async function clientLoader({
-  params,
-}: Route.ClientLoaderArgs) {
-    const pendingAction = window.sessionStorage.getItem("x-century-pending-action");
-    const pendingUrl = window.sessionStorage.getItem("x-century-pending-url");
 
-    if (pendingAction && pendingUrl) {
-        const parsedAction = JSON.parse(pendingAction);
+export async function clientLoader({
+    serverLoader,
+    params,
+}: Route.ClientLoaderArgs) {
+    const serverData = await serverLoader();
+    const url = new URL(serverData.url);
+    const pendingAction = window.sessionStorage.getItem("x-century-pending-action");
+
+    if (pendingAction && !url.searchParams.get("error")) {
+        const { action, id, payload } = JSON.parse(pendingAction);
         window.sessionStorage.removeItem("x-century-pending-action");
-        window.sessionStorage.removeItem("x-century-pending-url");
-        console.log(parsedAction, pendingUrl);
+        if (action === "vote") {
+            const dir = payload === "0" ? "1" : payload;
+            const fetcher = await fetch(`/api/upvote/${id}/${dir}`, {
+                method: "POST",
+            });
+            const fetcherData = await fetcher.json();
+            if (fetcherData.error) {
+                addToast({ 
+                    title: "Error", 
+                    description: "There was an error processing your vote.",
+                    color: "danger", 
+                });
+            } else {
+                addToast({ 
+                    title: "Success", 
+                    description: "Your vote was processed successfully.",
+                    color: "success", 
+                });
+            }
+        }
     }
+    return {
+        ...serverData,
+    }
+}
+clientLoader.hydrate = true as const;
+export function HydrateFallback() {
+    return (
+        <section className="flex flex-col items-center gap-5 w-full mb-5">
+            <Spinner variant="wave" color="primary" size="lg" />
+        </section>
+    )
 }
 export default function Index({ loaderData }: Route.ComponentProps) {
     const { render, renderLoadingDots } = useInfiniteScroll(loaderData.data);
@@ -35,8 +67,7 @@ export default function Index({ loaderData }: Route.ComponentProps) {
     useEffect(() => {
         const url = new URL(loaderData.url);
         const error = url.searchParams.get("error");
-        console.log(error)
-        if (error) {
+        if (error && error.includes("access_denied")) {
             addToast({ 
                 title: "Authorization Error", 
                 description: "You need to authorize the app to upvote and comment.",
