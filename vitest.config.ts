9b8fafd016fe33190d937e763a0be9ffdccd797e
Jossy Solá <jossysola@gmail.com>
2026-04-21 01:52:49 -0600

    Reestructures config file

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,48 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
-import { playwright } from '@vitest/browser-playwright'
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import { playwright } from '@vitest/browser-playwright';
+import tsconfigPaths from "vite-tsconfig-paths";
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const paths = tsconfigPaths({ root: __dirname });
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tsconfigPaths({root: resolve(__dirname, "../../")}), react()],
   test: {
-    browser: {
-      enabled: true,
-      provider: playwright(),
-      instances: [
-        { browser: 'chromium' },
-      ],
-    }
+    projects: [
+      {
+        plugins: [paths],
+        test: {
+          include: ['app/test/**/*.server.test.ts'],
+          name: 'Server-side',
+          environment: 'node',
+        }
+      },
+      {
+        plugins: [paths],
+        test: {
+          include: ['app/test/**/*.browser.test.{ts,tsx}'],
+          name: 'Browser',
+          browser: {
+            enabled: true,
+            provider: playwright(),
+            instances: [
+              { browser: 'chromium' },
+              { browser: 'firefox' },
+            ]
+          }
+        }
+      },
+      {
+        plugins: [paths],
+        test: {
+          include: ['app/test/**/*.client.test.ts'],
+          name: 'Client-side',
+          environment: 'happy-dom',
+        }
+      }
+
+    ]
   }
 })
