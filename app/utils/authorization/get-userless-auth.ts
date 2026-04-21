c2656fce6cd14c803ee6384dbf52bc417ce023b5
Jossy Solá <jossysola@gmail.com>
2026-04-21 01:54:28 -0600

    Creates util folder for authorization flows

---
+++ app/utils/authorization/get-userless-auth.ts
@@ -0,0 +1,30 @@
+import type { OnlyAppAuthResponse } from "../types";
+
+export default async function getUserlessAuthorization() {
+    try {
+        const client_id = process.env.REDDIT_CLIENT_ID;
+        const client_secret = process.env.REDDIT_CLIENT_SECRET;
+        // Encode both env variables into base64
+        const encode = Buffer.from(client_id + ':' + client_secret).toString('base64');
+        // Request access token using 'Application Only OAuth' flow
+        const req = await fetch("https://www.reddit.com/api/v1/access_token", {
+            method: 'POST',
+            headers: {
+                Authorization: `Basic ${encode}`,
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'User-Agent': 'web:centurytimes:v2.1.0 (by /u/jossysola)'
+            },
+            body: new URLSearchParams({
+                grant_type: 'client_credentials',
+            })
+        });
+        if (!req.ok) {
+            throw new Error(`Error at access_token endpoint. ${req.statusText}`)
+        }
+        const response: OnlyAppAuthResponse = await req.json();
+        return response;
+    } catch (error: any) {
+        console.error(error);
+        throw new Error(error.message);
+    }
+}
