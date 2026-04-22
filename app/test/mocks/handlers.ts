031ac6ac1e886a4dc993f54461ec638b9cf85cba
Jossy Solá <jossysola@gmail.com>
2026-04-21 22:39:33 -0600

    Adds refresh_token to returned object

--- app/test/mocks/handlers.ts
+++ app/test/mocks/handlers.ts
@@ -3,10 +3,11 @@ import { http, HttpResponse } from 'msw';
 export const handlers = [
     http.post('https://www.reddit.com/api/v1/access_token', () => {
         return HttpResponse.json({
-            "access_token": "acessToken123",
+            "access_token": "accessToken123",
             "token_type": "bearer",
             "expires_in": 36000,
             "scope": "read",
+            "refresh_token": "refreshToken123"
         })
     }),
 ]
