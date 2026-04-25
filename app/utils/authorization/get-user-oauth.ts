ed1a49c9fce85f987796e9b75c5ad3dc6bbd90c8
Jossy Solá <jossysola@gmail.com>
2026-04-24 19:42:41 -0600

    Creates utility function

---
+++ app/utils/authorization/get-user-oauth.ts
@@ -0,0 +1,20 @@
+export default async function getUserOAuth(state: string) {
+    try {
+        if (!state) throw new Error("Empty state");
+        const client_id = process.env.REDDIT_CLIENT_ID;
+        if (!client_id) throw new Error("Empty client id");
+        const endpoint = new URL("https://www.reddit.com/api/v1/authorize");
+        endpoint.search = new URLSearchParams({
+            client_id,
+            response_type: "code",
+            state,
+            redirect_uri: "http://localhost:3000",
+            duration: "permanent",
+            scope: "edit identity read submit vote"
+        }).toString();
+        return endpoint;
+    } catch (error: any) {
+        console.error("Error at getUserOAuth", error.message);
+        throw new Error("Error attempting to sign in");
+    }
+}
