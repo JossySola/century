97629e68d33b3acc3deb15a52eedcaad098f8a67
Jossy Solá <jossysola@gmail.com>
2026-05-08 16:44:16 -0600

    Passes props to RedditSignDropdown

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,13 +1,12 @@
 import { commitSession, getSession } from "~/sessions.server";
 import type { Route } from "./+types/_index";
-import useInfiniteScroll from "~/utils/custom-hooks";
-import { useEffect } from "react";
-import { addToast, Spinner } from "@heroui/react";
+import { Spinner } from "@heroui/react";
 import searchByCategory from "~/utils/querying/search-by-category";
 import getMe from "~/utils/querying/get-me";
 import RedditSignDropdown from "~/ui/dropdown/sign";
 import getUserOAuth from "~/utils/authorization/get-user-oauth";
 import { redirect } from "react-router";
+import type { IdentityResponse } from "~/utils/types";
 
 export async function loader({ request }: Route.LoaderArgs) {
     const session = await getSession(
@@ -16,51 +15,21 @@ export async function loader({ request }: Route.LoaderArgs) {
     const access_token = session.get("access_token");
     if (!access_token) return {};
     const subreddits = await searchByCategory("worldnews", access_token);
-    const me = await getMe(access_token);
+    const identity: IdentityResponse = await getMe(access_token);
+    const me = {
+        name: identity.name ?? undefined,
+        icon_img: identity.icon_img ?? undefined,
+        name_prefixed: identity.subreddit?.display_name_prefixed ?? undefined,
+        icon_color: identity.subreddit?.icon_color ?? undefined,
+        total_karma: identity.total_karma ?? undefined,
+        gold_creddits: identity.gold_creddits ?? undefined,
+        subscribers: identity.subreddit?.subscribers ?? 0,
+    }
     return {
         subreddits: subreddits ?? null,
-        me: me ?? null,
+        me,
     };
 }
-/*
-export async function clientLoader({
-    serverLoader,
-    params,
-}: Route.ClientLoaderArgs) {
-    const serverData = await serverLoader();
-    const url = new URL(serverData.url);
-    const pendingAction = window.sessionStorage.getItem("x-century-pending-action");
-
-    if (pendingAction && !url.searchParams.get("error")) {
-        const { action, id, payload } = JSON.parse(pendingAction);
-        if (action === "vote") {
-            const dir = payload === "0" ? "1" : payload;
-            const fetcher = await fetch(`/api/upvote/${id}/${dir}`, {
-                method: "POST",
-            });
-            const fetcherData = await fetcher.json();
-            if (fetcherData.error) {
-                addToast({ 
-                    title: "Error", 
-                    description: "There was an error processing your vote.",
-                    color: "danger", 
-                });
-            } else {
-                addToast({ 
-                    title: "Success", 
-                    description: "Your vote was processed successfully.",
-                    color: "success", 
-                });
-                window.sessionStorage.removeItem("x-century-pending-action");
-            }
-        }
-    }
-    return {
-        ...serverData,
-    }
-}
-clientLoader.hydrate = true as const;
-*/
 export async function action({ request }: Route.ActionArgs) {
     const session = await getSession(
         request.headers.get("Cookie"),
@@ -74,25 +43,20 @@ export async function action({ request }: Route.ActionArgs) {
         }
     });
 }
-export default function Index({ loaderData }: Route.ComponentProps) {
-    console.log(loaderData)
-    /*
-    useEffect(() => {
-        const url = new URL(loaderData.url);
-        const error = url.searchParams.get("error");
-        if (error && error.includes("access_denied")) {
-            addToast({ 
-                title: "Authorization Error", 
-                description: "You need to authorize the app to upvote and comment.",
-                color: "danger", 
-            });
-        }
-    }, []);
-    */
+export default function Index({ loaderData, actionData }: Route.ComponentProps) {
+    const identity = loaderData.me;
+    //console.log(actionData)
     return (
         <main className="flex flex-col items-center gap-5 w-full mb-5">
             <div className="absolute top-7 right-[8vw] z-10" aria-label="Sign into Reddit">
-                
+                <RedditSignDropdown
+                name={identity?.name} 
+                display_name={identity?.name_prefixed} 
+                icon_img={identity?.icon_img}
+                icon_color={identity?.icon_color}
+                total_karma={identity?.total_karma}
+                gold_creddits={identity?.gold_creddits}
+                subscribers={identity?.subscribers} />
             </div>
         </main>
     )
