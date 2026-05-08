2b5ca877c756ef2e426833c0a5499587a641141c
Jossy Solá <jossysola@gmail.com>
2026-05-08 16:42:42 -0600

    Adds props and redirects user to /signout when pressing link

--- app/ui/dropdown/sign.tsx
+++ app/ui/dropdown/sign.tsx
@@ -1,12 +1,16 @@
 import { Avatar, Badge, Button, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, User } from "@heroui/react";
 import { Logout } from "../icons";
-import { Form } from "react-router";
+import { Form, Link, redirect } from "react-router";
 
 export default function RedditSignDropdown({
     name,
     icon_img,
     display_name,
-}: { name: string, icon_img: string | null, display_name: string }) {
+    icon_color,
+    total_karma,
+    gold_creddits,
+    subscribers,
+}: { name?: string, icon_img?: string | null, display_name?: string, icon_color?: string, total_karma?: number, gold_creddits?: number, subscribers?: number }) {
     return (
         <Dropdown>
             <DropdownTrigger>
@@ -29,22 +33,27 @@ export default function RedditSignDropdown({
                         <DropdownItem key="user-info" className="data-[hover=true]:bg-transparent">
                             <User
                             avatarProps={{
-                                src: icon_img ?? ""
+                                src: icon_img ?? "",
                             }}
                             description={display_name ?? ""}
                             name={name} />
                         </DropdownItem>
+                        <DropdownItem key="user-subscribers">
+                            <p className="text-gray-600">Subscribers: {subscribers}</p>
+                        </DropdownItem>
+                        <DropdownItem key="user-karma">
+                            <p className="text-gray-600">Karma: {total_karma}</p>
+                        </DropdownItem>
+                        <DropdownItem key="user-gold-creddits">
+                            <p className="text-gray-600">Gold Creddits: {gold_creddits}</p>
+                        </DropdownItem>
                         <DropdownItem key="sign-out" className="data-[hover=true]:bg-transparent">
-                            <Button
-                            className="w-full bg-[#FF4500] text-white hover:bg-[#e03d02] active:bg-[#B32D00] focus:ring-[#FF4500] font-medium"
-                            radius="full" 
-                            endContent={ <Logout /> }
-                            onPress={async () => {
-                                await fetch("/api/signout", { method: "POST" });
-                                window.location.reload();
-                            }}>
+                            <Link
+                            className="bg-[#FF4500] text-white hover:bg-[#e03d02] active:bg-[#B32D00] focus:ring-[#FF4500] font-medium rounded-2xl p-2 flex flex-row gap-2 w-fit justify-center items-center"
+                            to="/signout">
                                 Sign out
-                            </Button>                        
+                                <Logout />
+                            </Link>
                         </DropdownItem>
                     </>
                     : <DropdownItem key="sign-in" className="data-[hover=true]:bg-transparent">
@@ -56,7 +65,6 @@ export default function RedditSignDropdown({
                                 Sign in with Reddit
                             </Button>
                         </Form>
-                       
                     </DropdownItem>
                 }
             </DropdownMenu>
