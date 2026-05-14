2129c1c4fcd8824dd81d7535068401b5125dc465
Jossy Solá <jossysola@gmail.com>
2026-05-14 12:23:50 -0600

    Implements server action: upvote

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,9 +1,12 @@
 import { getSession } from "~/sessions.server";
 import type { Route } from "./+types/_index";
-import { Spinner } from "@heroui/react";
+import { addToast, Spinner } from "@heroui/react";
 import searchByCategory from "~/utils/querying/search-by-category";
 import type { T3 as t3Type } from "~/utils/types";
 import T3 from "~/ui/modals/t3";
+import upvoteAction from "~/utils/actions/upvote";
+import { useEffect } from "react";
+import { useFetchers } from "react-router";
 
 export async function loader({ request }: Route.LoaderArgs) {
     const session = await getSession(
@@ -16,11 +19,63 @@ export async function loader({ request }: Route.LoaderArgs) {
         subreddits: subreddits ?? null,
     };
 }
-export async function action({ request }: Route.ActionArgs) {
 
+export async function action({ request }: Route.ActionArgs) {
+    const session = await getSession(
+        request.headers.get("Cookie"),
+    );
+    const access_token = session.get("access_token");
+    const formData = await request.formData();
+    const action = formData.get("action");
+    if (!access_token) {
+        return { error: "You must sign in to Reddit in order to upvote" };
+    }
+    if (action === "upvote") {
+        const dir = (formData.get("dir")?.toString() ?? "") as "1" | "0" | "-1";
+        const id = formData.get("id")?.toString() ?? "";
+        try {
+            const ok = await upvoteAction(access_token, id, dir);
+            if (!ok) return { error: "Unable to register vote" };
+            return { ok: true };
+        } catch(e: any) {
+            console.log(e.message);
+            if (e.message.includes("Unauthorized")) {
+                return { error: "You must sign in to Reddit in order to upvote" };
+            } else {
+                return { error: "Unable to register vote" };
+            }
+        }
+        
+    }
+    return null;
 }
+
 export default function Index({ loaderData, actionData }: Route.ComponentProps) {
-    const children: Array<t3Type> = loaderData.subreddits.data.children;
+    const children: Array<t3Type> = loaderData.subreddits?.data?.children ?? []; 
+    const fetchers = useFetchers();
+
+    useEffect(() => {
+        if (actionData && actionData.error) {
+            addToast({
+                description: actionData.error,
+                color: "danger",
+            });
+        }
+    }, [actionData]);
+
+    useEffect(() => {
+        // Using fetcher from Upvote UI component does not make data available through actionData
+        for (const fetcher of fetchers) {
+            if (fetcher.data && fetcher.data.error) {
+                addToast({
+                    description: fetcher.data.error,
+                    color: "danger",
+                });
+                break;
+            }
+        }
+    }, [fetchers]);
+
     return (
         <main className="flex flex-col items-center gap-5 w-full mb-5">
             {
@@ -49,6 +104,7 @@ export default function Index({ loaderData, actionData }: Route.ComponentProps) 
         </main>
     )
 }
+
 export function HydrateFallback() {
     return (
         <section className="flex flex-col items-center gap-5 w-full mb-5">
