34357600c16db0118b5a1a938637d68b5447dd73
Jossy Solá <jossysola@gmail.com>
2026-05-14 17:01:02 -0600

    Update

--- build/client/assets/features-animation-BiNssv1S.js
+++
@@ -1,1 +0,0 @@
-import{g as a,a as e,c as n}from"./gestures-D597FSm6.js";const s={renderer:n,...e,...a};export{s as d};
--- build/client/assets/src-UW24ZMRV-C-k5Ddun.js
+++
@@ -1,1 +0,0 @@
-import{d as o}from"./features-animation-BiNssv1S.js";import"./gestures-D597FSm6.js";import"./index-D7seX151.js";import"./icons-CibqzyVO.js";var i=o;export{i as default};
