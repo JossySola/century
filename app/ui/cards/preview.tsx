e0c9a6697cf0ff58c16b0b6983df6b773410cf25
Jossy Solá <jossysola@gmail.com>
2026-05-14 16:50:28 -0600

    Dynamically renders red Heart if user upvoted post

--- app/ui/cards/preview.tsx
+++ app/ui/cards/preview.tsx
@@ -1,5 +1,5 @@
 import { Card, CardBody, CardFooter, CardHeader, Chip, Divider, Image, User } from "@heroui/react";
-import { Heart, Message } from "../icons";
+import { Heart, HeartFill, Message } from "../icons";
 import { formatAmount } from "~/utils/formatting/format-amount";
 
 export default function Preview(
@@ -75,7 +75,9 @@ export default function Preview(
             <Divider />
             <CardFooter className="flex flex-row justify-center items-center gap-5">
                 <div className="flex flex-row justify-center items-center gap-2">
-                    <Heart />
+                    {likes
+                    ? <HeartFill color="oklch(57.7% 0.245 27.325)"/> 
+                    : <Heart />}
                     <span>{formatAmount(ups)}</span>
                 </div>
                 
