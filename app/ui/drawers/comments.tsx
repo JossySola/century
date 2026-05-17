800d937053cbd8c6542d223bb1f3f69ba2773b41
Jossy Solá <jossysola@gmail.com>
2026-05-17 10:38:09 -0600

    Starts implementing Breadcrumbs with pagination

--- app/ui/drawers/comments.tsx
+++ app/ui/drawers/comments.tsx
@@ -1,127 +1,92 @@
-import { addToast, Button, Drawer, DrawerBody, DrawerContent, DrawerFooter, DrawerHeader, Spinner, useDisclosure } from "@heroui/react";
-import type { Listing, Thing } from "~/utils/types";
+import { addToast, BreadcrumbItem, Breadcrumbs, Button, Drawer, DrawerBody, DrawerContent, DrawerFooter, DrawerHeader, Spinner, useDisclosure } from "@heroui/react";
+import type { T1 as CommentKind, Listing, More, Thing } from "~/utils/types";
 import T1 from "../cards/t1";
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BookOpen } from "../icons";
+import { useFetcher } from "react-router";
 
-export default function Comments({ num_comments, comments }: {
+export default function Comments({ permalink, num_comments }: {
+    permalink: string,
     num_comments: number,
-    comments?: Listing,
 }) {
+    const fetcher = useFetcher();
+    const [currentPage, setCurrentPage] = useState<React.Key>("");
+    const [listings, setListings] = useState<Array<Listing>>([]);
     const {isOpen, onOpen, onOpenChange} = useDisclosure();
-    const [feed, setFeed] = useState<Array<Thing>>([]);
-    const [isLoading, setIsLoading] = useState<boolean>(false);
-    const children = comments && comments.data ? comments.data.children : [];
-    const loadingRef = useRef(null);
-    const scrollableRef = useRef<HTMLDivElement>(null);
-    const scrollPositionRef = useRef(0);
-    
     useEffect(() => {
-        if (!isOpen) return;
-        if (comments && children) {
-            if (num_comments && children.length < num_comments) {
-                addToast({
-                    title: "Some comments may not be displayed because the user or comment has been deleted.",
-                    color: "warning",
-                    size: "lg"
-                })
+        if (num_comments > 0) {
+            fetcher.load(`api${permalink}`);
+        }
+    }, []);
+    useEffect(() => {
+        if (fetcher.data) {
+            if (fetcher.data[1] && !listings.length) {
+                setListings([fetcher.data[1]]);
             }
         }
-    }, [isOpen])
-    useEffect(() => {
-        if (!comments) return;
-        if (feed.length > 0) return;
-        loadComments();
-    }, [children.length]);    
-    useEffect(() => {
-        if (!isOpen) return;
-        if (!scrollableRef.current || !loadingRef.current) return;
-
-        const observer = new IntersectionObserver(
-            ([entry]) => {
-                if (entry.isIntersecting) {
-                    loadComments();
-                }
-            }, 
-            {
-                root: scrollableRef.current,
-                threshold: 1,
-            }
-        );
-        if (loadingRef.current) {
-            observer.observe(loadingRef.current);
-        }
-        return () => observer.disconnect();
-    }, [isOpen, feed.length]);
-    useEffect(() => {
-        if (!isLoading) {
-            requestAnimationFrame(() => {
-                restoreScrollPosition();
-            })
-        }
-    }, [feed, isLoading]);
-
-    const loadComments = () => {
-        setIsLoading(true);
-        saveScrollPosition();
-        setFeed(prev => {
-            const diff = children.length - prev.length;
-            if ((children.length && children.length <= 5) || diff < 5) {
-                return children;
-            }
-            const count = prev.length + 5;
-            return children.slice(0, count);
-        })
-        setIsLoading(false);
-    };
-    const saveScrollPosition = () => {
-        if (scrollableRef.current) {
-            scrollPositionRef.current = scrollableRef.current.scrollTop;
-        }
-    };
-    const restoreScrollPosition = () => {
-        if (scrollableRef.current) {
-            scrollableRef.current.scrollTop = scrollPositionRef.current;
-        }
-    };
-    
+    }, [fetcher.data]);
+    const addRepliesToStack = (replies: Listing) => {
+        setListings(prev => {
+            const newArr = [...prev];
+            prev.push(replies);
+            return newArr;
+        });
+    }
     return (
         <>
-        <Button onPress={onOpen} isDisabled={ num_comments === 0 } size="lg" className="w-full flex flex-row p-2" color="danger"><span className="text-lg">Read comments </span><BookOpen width={20} height={20} /></Button>
+        <Button onPress={onOpen} isDisabled={ num_comments === 0 } size="lg" className="w-full flex flex-row p-2" color="danger">
+            <span className="text-lg">Read comments </span>
+            <BookOpen width={20} height={20} />
+        </Button>
         <Drawer isOpen={isOpen} onOpenChange={onOpenChange} placement="bottom" size="lg">
             <DrawerContent>
                 {
                     onClose => (
                         <>
                         <DrawerHeader></DrawerHeader>
-                        <DrawerBody>
-                            <div ref={scrollableRef} className="flex flex-col-reverse items-center overflow-y-auto h-full p-x-5 pb-5">
-                                {
-                                    isLoading && <Spinner size="lg" color="primary" />
-                                }
-                                <section className="w-5/6 max-w-[95%] flex flex-col-reverse justify-center items-center gap-2">
-                                    {
-                                        feed
-                                        ? feed.map((comment, index) => {
-                                            if (comment.kind === "t1") {
-                                                return <T1 
-                                                comment={comment} 
-                                                key={comment.data.id} 
-                                                isOpen={isOpen} 
-                                                index={index} 
-                                                fullname={comment.data.name} />
-                                            }
-                                        })
-                                        :   <span>No comments yet</span>
+                        <DrawerBody className="w-full flex flex-col justify-center items-center">
+                            <Breadcrumbs size="lg" underline="active" onAction={key => setCurrentPage(key)}>
+                            {
+                                listings && listings.map(listing => {
+                                    const array = listing.data.children;
+                                    const lastItem = array.splice(-1, array.length - 1);
+                                    const id = lastItem[0].kind === "more" ? lastItem[0].data.id : "";
+                                    if (id) {
+                                        return (
+                                            <BreadcrumbItem key={id}>
+                                                {
+                                                    array && array.map(comment => {
+                                                        if (comment.kind === "t1") {
+                                                            return (
+                                                                <T1 
+                                                                key={comment.data.id}
+                                                                author={comment.data.author}
+                                                                author_fullname={comment.data.author_fullname}
+                                                                body={comment.data.body}
+                                                                body_html={comment.data.body_html}
+                                                                created_utc={comment.data.created_utc}
+                                                                depth={comment.data.depth}
+                                                                downs={comment.data.downs}
+                                                                likes={comment.data.likes}
+                                                                is_submitter={comment.data.is_submitter}
+                                                                link_id={comment.data.link_id}
+                                                                name={comment.data.name}
+                                                                replies={comment.data.replies}
+                                                                send_replies={comment.data.send_replies}
+                                                                subreddit_id={comment.data.subreddit_id}
+                                                                ups={comment.data.ups} 
+                                                                addRepliesToStack={addRepliesToStack} />
+                                                            )
+                                                        }
+                                                    })
+                                                }
+                                            </BreadcrumbItem>
+                                        )
                                     }
-                                </section>
-                                {
-                                    comments && comments.data && comments.data.children && comments.data.children.length !== feed.length 
-                                    && <div ref={loadingRef} className="flex flex-row justify-center items-center w-full h-fit mb-10">
-                                        <Spinner variant="wave" color="primary" size="lg" />
-                                    </div>
-                                }
-                            </div>
+                                    return;
+                                })
+                            }
+                            </Breadcrumbs>
                         </DrawerBody>
                         <DrawerFooter>
                             <Button onPress={onClose}><span>Close</span></Button>
