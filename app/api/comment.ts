fe311304ffede3a97668e261057226b9363bb8ca
Jossy Solá <jossysola@gmail.com>
2026-05-19 11:37:54 -0600

    Completes API endpoint

--- app/api/comment.ts
+++ app/api/comment.ts
@@ -8,24 +8,38 @@ export async function action({ request, params }: Route.ActionArgs) {
             request.headers.get("Cookie")
         );
         const access_token = session.get("access_token");
-        if (!access_token) throw new Error("Access token missing.");
+        if (!access_token) throw new Error("Unauthorized");
+        const form = await request.formData();
+        const text = String(form.get("text") ?? "");
+        if (!text) throw new Error("Empty input");
         const req = await fetch("https://oauth.reddit.com/api/comment", {
             method: 'POST',
             headers: {
-                'Authorization': `Basic ${access_token}`,
-                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${access_token}`,
+                'Content-Type': 'application/x-www-form-urlencoded',
                 'User-Agent': 'web:centurytimes:v2.1.0 (by /u/jossysola)',
             },
             body: new URLSearchParams({
                 api_type: "json",
                 return_rtjson: "true",
-                text: "",
+                text,
                 thing_id: params.id,
             })
         });
         if (!req.ok) throw new Error(`${req.statusText}`);
+        const data = await req.json();
+        console.log(data)
+        return {
+            data,
+        }
     } catch (error: any) {
-        console.error(error.message);
-        throw new Error(`Failed at /api/comment: ${error.message}`);
+        console.error(`Failed at /api/comment: ${error.message}`);
+        if (error.message.includes("Unauthorized")) {
+            return { error: "You must sign in to Reddit first" }
+        } else if (error.message.includes("Empty input")) {
+            return { error: "The comment input cannot be empty" }
+        } else {
+            return { error: "Unable to post comment" }
+        }
     }
 }
