b91f6d1c1beeeabecbe726783126f3204b9643fc
Jossy Solá <jossysola@gmail.com>
2026-05-18 21:27:09 -0600

    Completes component

--- app/ui/drawers/comments.tsx
+++ app/ui/drawers/comments.tsx
@@ -1,7 +1,7 @@
-import { addToast, BreadcrumbItem, Breadcrumbs, Button, Drawer, DrawerBody, DrawerContent, DrawerFooter, DrawerHeader, Spinner, useDisclosure } from "@heroui/react";
-import type { T1 as CommentKind, Listing, More, Thing } from "~/utils/types";
+import { Button, Drawer, DrawerBody, DrawerContent, DrawerFooter, DrawerHeader, Spinner, useDisclosure } from "@heroui/react";
+import type { T1 as CommentKind } from "~/utils/types";
 import T1 from "../cards/t1";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { BookOpen } from "../icons";
 import { useFetcher } from "react-router";
 
@@ -10,8 +10,7 @@ export default function Comments({ permalink, num_comments }: {
     num_comments: number,
 }) {
     const fetcher = useFetcher();
-    const [currentPage, setCurrentPage] = useState<React.Key>("");
-    const [listings, setListings] = useState<Array<Listing>>([]);
+    const [comments, setComments] = useState<Array<CommentKind>>([]);
     const {isOpen, onOpen, onOpenChange} = useDisclosure();
     useEffect(() => {
         if (num_comments > 0) {
@@ -20,82 +19,58 @@ export default function Comments({ permalink, num_comments }: {
     }, []);
     useEffect(() => {
         if (fetcher.data) {
-            if (fetcher.data[1] && !listings.length) {
-                setListings([fetcher.data[1]]);
+            if (fetcher.data[1] && fetcher.data[1].kind === "Listing") {
+                setComments(fetcher.data[1].data.children);
             }
         }
     }, [fetcher.data]);
-    const addRepliesToStack = (replies: Listing) => {
-        setListings(prev => {
-            const newArr = [...prev];
-            prev.push(replies);
-            return newArr;
-        });
-    }
     return (
         <>
-        <Button onPress={onOpen} isDisabled={ num_comments === 0 } size="lg" className="w-full flex flex-row p-2" color="danger">
-            <span className="text-lg">Read comments </span>
-            <BookOpen width={20} height={20} />
-        </Button>
-        <Drawer isOpen={isOpen} onOpenChange={onOpenChange} placement="bottom" size="lg">
-            <DrawerContent>
-                {
-                    onClose => (
-                        <>
-                        <DrawerHeader></DrawerHeader>
-                        <DrawerBody className="w-full flex flex-col justify-center items-center">
-                            <Breadcrumbs size="lg" underline="active" onAction={key => setCurrentPage(key)}>
-                            {
-                                listings && listings.map(listing => {
-                                    const array = listing.data.children;
-                                    const lastItem = array.splice(-1, array.length - 1);
-                                    const id = lastItem[0].kind === "more" ? lastItem[0].data.id : "";
-                                    if (id) {
-                                        return (
-                                            <BreadcrumbItem key={id}>
-                                                {
-                                                    array && array.map(comment => {
-                                                        if (comment.kind === "t1") {
-                                                            return (
-                                                                <T1 
-                                                                key={comment.data.id}
-                                                                author={comment.data.author}
-                                                                author_fullname={comment.data.author_fullname}
-                                                                body={comment.data.body}
-                                                                body_html={comment.data.body_html}
-                                                                created_utc={comment.data.created_utc}
-                                                                depth={comment.data.depth}
-                                                                downs={comment.data.downs}
-                                                                likes={comment.data.likes}
-                                                                is_submitter={comment.data.is_submitter}
-                                                                link_id={comment.data.link_id}
-                                                                name={comment.data.name}
-                                                                replies={comment.data.replies}
-                                                                send_replies={comment.data.send_replies}
-                                                                subreddit_id={comment.data.subreddit_id}
-                                                                ups={comment.data.ups} 
-                                                                addRepliesToStack={addRepliesToStack} />
-                                                            )
-                                                        }
-                                                    })
-                                                }
-                                            </BreadcrumbItem>
-                                        )
+            <Button onPress={onOpen} isDisabled={ num_comments === 0 } size="lg" className="w-full flex flex-row p-2" color="danger">
+                <span className="text-lg">Read comments </span>
+                <BookOpen width={20} height={20} />
+            </Button>
+            <Drawer isOpen={isOpen} onOpenChange={onOpenChange} placement="bottom" size="lg">
+                <DrawerContent>
+                    {
+                        onClose => (
+                            <>
+                                <DrawerHeader></DrawerHeader>
+                                <DrawerBody className="w-full flex flex-col  gap-3">
+                                    {
+                                        comments && comments.length
+                                        ?   comments.map(comment => {
+                                                if (comment.kind === "t1") {
+                                                return <T1 
+                                                key={comment.data.id}
+                                                author={comment.data.author}
+                                                author_fullname={comment.data.author_fullname}
+                                                body={comment.data.body}
+                                                body_html={comment.data.body_html}
+                                                created_utc={comment.data.created_utc}
+                                                depth={comment.data.depth}
+                                                downs={comment.data.downs}
+                                                likes={comment.data.likes}
+                                                is_submitter={comment.data.is_submitter}
+                                                link_id={comment.data.link_id}
+                                                name={comment.data.name}
+                                                replies={comment.data.replies}
+                                                send_replies={comment.data.send_replies}
+                                                subreddit_id={comment.data.subreddit_id}
+                                                ups={comment.data.ups} />
+                                            }
+                                        })
+                                        : null
                                     }
-                                    return;
-                                })
-                            }
-                            </Breadcrumbs>
-                        </DrawerBody>
-                        <DrawerFooter>
-                            <Button onPress={onClose}><span>Close</span></Button>
-                        </DrawerFooter>
-                        </>
-                    )
-                }
-            </DrawerContent>
-        </Drawer>
+                                </DrawerBody>
+                                <DrawerFooter>
+                                    <Button onPress={onClose}><span>Close</span></Button>
+                                </DrawerFooter>
+                            </>
+                        )
+                    }
+                </DrawerContent>
+            </Drawer>
         </>
     )
 }
