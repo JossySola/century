1e8e898e84a700d87be6b486fefd2f44af5fca5b
Jossy Solá <jossysola@gmail.com>
2026-05-18 23:12:10 -0600

    Updates api routes

--- app/routes.ts
+++ app/routes.ts
@@ -14,10 +14,10 @@ export default [
     ...prefix("api", [
         route("r/*", "./api/comments.ts"),
         route("comment/:id", "./api/comment.ts"),
-        route("vote/:id/:vote", "./api/vote.ts"),
+        route("vote/:id", "./api/vote.ts"),
         route("del/:id", "./api/del.ts"),
         route("editusertext/:id", "./api/editusertext.ts"),
-        route("save/:id/:category", "./api/save.ts"),
+        route("save/:id", "./api/save.ts"),
         route("unsave/:id", "./api/unsave.ts"),
         route("saved_categories", "./api/saved_categories.ts"),
     ]),
