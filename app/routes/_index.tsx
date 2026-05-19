e32f18d61c8bcf3275f4d1429f145c59c5e8a030
Jossy Solá <jossysola@gmail.com>
2026-05-18 23:11:26 -0600

    Removes upvote action logic

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,12 +1,9 @@
 import { getSession } from "~/sessions.server";
 import type { Route } from "./+types/_index";
-import { addToast, Spinner } from "@heroui/react";
+import { Spinner } from "@heroui/react";
 import searchByCategory from "~/utils/querying/search-by-category";
 import type { T3 as t3Type } from "~/utils/types";
 import T3 from "~/ui/modals/t3";
-import upvoteAction from "~/utils/actions/upvote";
-import { useEffect } from "react";
-import { useFetchers } from "react-router";
 
 export async function loader({ request }: Route.LoaderArgs) {
     const session = await getSession(
@@ -25,55 +22,10 @@ export async function action({ request }: Route.ActionArgs) {
         request.headers.get("Cookie"),
     );
     const access_token = session.get("access_token");
-    const formData = await request.formData();
-    const action = formData.get("action");
-    if (!access_token) {
-        return { error: "You must sign in to Reddit in order to upvote" };
-    }
-    if (action === "upvote") {
-        const dir = (formData.get("dir")?.toString() ?? "") as "1" | "0" | "-1";
-        const id = formData.get("id")?.toString() ?? "";
-        try {
-            const ok = await upvoteAction(access_token, id, dir);
-            if (!ok) return { error: "Unable to register vote" };
-            return { ok: true };
-        } catch(e: any) {
-            console.log(e.message);
-            if (e.message.includes("Unauthorized")) {
-                return { error: "You must sign in to Reddit in order to upvote" };
-            } else {
-                return { error: "Unable to register vote" };
-            }
-        }
-    }
-    return null;
 }
 
 export default function Index({ loaderData, actionData }: Route.ComponentProps) {
     const children: Array<t3Type> = loaderData.subreddits?.data?.children ?? []; 
-    const fetchers = useFetchers();
-
-    useEffect(() => {
-        if (actionData && actionData.error) {
-            addToast({
-                description: actionData.error,
-                color: "danger",
-            });
-        }
-    }, [actionData]);
-
-    useEffect(() => {
-        // Using fetcher from Upvote UI component does not make data available through actionData
-        for (const fetcher of fetchers) {
-            if (fetcher.data && fetcher.data.error) {
-                addToast({
-                    description: fetcher.data.error,
-                    color: "danger",
-                });
-                break;
-            }
-        }
-    }, [fetchers]);
 
     return (
         <main className="flex flex-col items-center gap-5 w-full mb-5">
