3fc4821dca10dc55ad64b128e9e0c98f435fd6cb
Jossy Solá <jossysola@gmail.com>
2026-05-21 17:28:28 -0600

    Creates skeleton for preview components

--- app/ui/skeletons/comment-skeleton.tsx
+++ app/ui/skeletons/comment-skeleton.tsx
@@ -1,6 +1,7 @@
 import { Skeleton } from "@heroui/react";
+import { memo } from "react";
 
-export default function CommentSkeleton() {
+const CommentSkeleton = memo(function () {
     return (
         <div className="w-full p-5 flex flex-col gap-3">
             <div className="max-w-75 w-full flex items-center gap-3">
@@ -18,4 +19,5 @@ export default function CommentSkeleton() {
             </div>
         </div>
     )
-}
+});
+export default CommentSkeleton;
