78d929f4432d9bd163320d469ea0e5e0c652c32e
Jossy Solá <jossysola@gmail.com>
2026-05-21 18:43:56 -0600

    Adds compact option to endpoint

--- app/utils/authorization/get-user-oauth.ts
+++ app/utils/authorization/get-user-oauth.ts
@@ -4,7 +4,7 @@ export default async function getUserOAuth(state: string): Promise<URL | Error> 
         if (!state) throw new Error("Empty state");
         const client_id = process.env.REDDIT_CLIENT_ID;
         if (!client_id) throw new Error("Empty client id");
-        const endpoint = new URL("https://www.reddit.com/api/v1/authorize");
+        const endpoint = new URL("https://www.reddit.com/api/v1/authorize.compact?");
         endpoint.search = new URLSearchParams({
             client_id,
             response_type: "code",
