59496313bcf8b67a01bda4981b3991f28f184744
Jossy Solá <jossysola@gmail.com>
2026-05-21 18:18:18 -0600

    Adds Card wrapper

--- app/ui/skeletons/comment-skeleton.tsx
+++ app/ui/skeletons/comment-skeleton.tsx
@@ -1,9 +1,9 @@
-import { Skeleton } from "@heroui/react";
+import { Card, Skeleton } from "@heroui/react";
 import { memo } from "react";
 
 const CommentSkeleton = memo(function () {
     return (
-        <div className="w-full p-5 flex flex-col gap-3">
+        <Card className="w-full p-5 flex flex-col gap-3">
             <div className="max-w-75 w-full flex items-center gap-3">
                 <div>
                     <Skeleton className="flex rounded-full w-8 h-8" />
@@ -17,7 +17,7 @@ const CommentSkeleton = memo(function () {
                 <Skeleton className="h-3 w-4/5 rounded-lg" />
                 <Skeleton className="h-3 w-2/5 rounded-lg" />
             </div>
-        </div>
+        </Card>
     )
 });
 export default CommentSkeleton;
